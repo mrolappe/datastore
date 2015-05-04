fdd2e77f1f17a832ba0b6bc87aa211180c11df3b
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-04 02:01:59 -0500

    Reformat example for style

--- org.clojure/clojure/1.6.0/clj/clojure.core/re-seq/examples/andy2.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/re-seq/examples/andy2.clj
@@ -1,6 +1,9 @@
 ;; Capture groups in the regex cause each returned match to be a
 ;; vector of matches.  See re-find for more examples.
-user=> (re-seq #"\S+:\d+" " RX pkts:18 err:5 drop:48")
-("pkts:18" "err:5" "drop:48")
-user=> (re-seq #"(\S+):(\d+)" " RX pkts:18 err:5 drop:48")
-(["pkts:18" "pkts" "18"] ["err:5" "err" "5"] ["drop:48" "drop" "48"])
+(re-seq #"\S+:\d+" " RX pkts:18 err:5 drop:48")
+;; => ("pkts:18" "err:5" "drop:48")
+
+(re-seq #"(\S+):(\d+)" " RX pkts:18 err:5 drop:48")
+;; => (["pkts:18" "pkts" "18"]
+;;     ["err:5" "err" "5"]
+;;     ["drop:48" "drop" "48"])
