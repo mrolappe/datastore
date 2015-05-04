503ac29d0dfcbbc8aeb02dd8438cb1cb3b4993f7
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-04 02:48:27 -0500

    Reformat example for style

--- org.clojure/clojure/1.4.0/clj/clojure.test/is/examples/388776526.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.test/is/examples/388776526.clj
@@ -1,12 +1,8 @@
-; Testing for thrown exceptions
+;; Testing for thrown exceptions. Verifies that the specified exception is
+;; thrown
+(is (thrown? ArithmeticException (/ 1 0)))
 
-; Verifies that the specified exception is thrown
-user=> (is (thrown? ArithmeticException (/ 1 0)))
-#<ArithmeticException java.lang.ArithmeticException: Divide by zero>
-
-; Verified that the exception is thrown, and that the error message matches the specified regular expression.
-user=> (is (thrown-with-msg? ArithmeticException #"Divide by zero"
-  #_=>                       (/ 1 0)))
-#<ArithmeticException java.lang.ArithmeticException: Divide by zero>
-user=> 
+;; Verified that the exception is thrown, and that the error message matches the
+;; specified regular expression.
+(is (thrown-with-msg? ArithmeticException #"Divide by zero" (/ 1 0)))
 
