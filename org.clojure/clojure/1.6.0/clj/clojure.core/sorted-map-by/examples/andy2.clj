ee7ea2ac04ddfe52eb8dbb5882efe421e5bb0315
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-04 02:31:35 -0500

    Reformat example for style

--- org.clojure/clojure/1.6.0/clj/clojure.core/sorted-map-by/examples/andy2.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/sorted-map-by/examples/andy2.clj
@@ -2,11 +2,12 @@
 ;; and not added as a separate key in the map.  the value associated with
 ;; the second equal key "lion" does replace the first value.
 
-user=> (require '[clojure.string :as str])
-nil
-user=> (defn case-insensitive-cmp [s1 s2]
-         (compare (str/lower-case s1) (str/lower-case s2)))
-#'user/case-insensitive-cmp
-user=> (sorted-map-by case-insensitive-cmp "lion" "normal lion"
-                      "Lion" "Orycteropus afer")
-{"lion" "Orycteropus afer"}
+(require '[clojure.string :as str])
+
+(defn case-insensitive-cmp [s1 s2]
+  (compare (str/lower-case s1) (str/lower-case s2)))
+;; => #'user/case-insensitive-cmp
+
+(sorted-map-by case-insensitive-cmp "lion" "normal lion"
+               "Lion" "Orycteropus afer")
+;; => {"lion" "Orycteropus afer"}
