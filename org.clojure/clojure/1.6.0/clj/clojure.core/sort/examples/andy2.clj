1d9c0e2874e8dc0d41811d9d23b97bdebeb9dade
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-04 02:06:03 -0500

    Reformat example for style

--- org.clojure/clojure/1.6.0/clj/clojure.core/sort/examples/andy2.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/sort/examples/andy2.clj
@@ -1,11 +1,20 @@
-user=> (def x (to-array [32 9 11]))
-#'user/x
-user=> (seq x)
-(32 9 11)
-user=> (sort x)   ; returns sorted sequence
-(9 11 32)
-user=> (seq x)    ; but also modifies Java array x
-(9 11 32)
-user=> (sort (aclone x))   ; can avoid this by copying the array
-(9 11 32)
-;; Such copying is unnecessary for args that are not a Java array
+(def x (to-array [32 9 11]))
+;; => #'user/x
+
+;; You can seq an array, the result being the sequence of its elements.
+(seq x)
+;; => (32 9 11)
+
+;; You can sort it to return the sequence in sorted order
+(sort x)
+;; => (9 11 32)
+
+;; But sorting also modifies the backing JVM array. Note that (seq x) changed.
+(seq x)
+;; => (9 11 32)
+
+;; One may avoid this by manually copying the array. Note that this is
+;; only required for mutable Java arrays, Clojure persistent
+;; collections will not present this behavior.
+(sort (aclone x))
+;; => (9 11 32)
