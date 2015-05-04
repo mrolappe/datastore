12aa35459dd94700ff43bc290028b29b9b82b3b8
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-04 01:52:13 -0500

    Reformat example for style

--- org.clojure/clojure/1.6.0/clj/clojure.core/subs/examples/andy1.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/subs/examples/andy1.clj
@@ -1,10 +1,16 @@
-user=> (subs "abcdef" 1 3)
-"bc"
-user=> (subs "abcdef" 1)
-"bcdef"
-user=> (subs "abcdef" 4 6)
-"ef"
-user=> (subs "abcdef" 4 7)
-StringIndexOutOfBoundsException String index out of range: 7  java.lang.String.substring (String.java:1907)
-user=> (subs "abcdef" 5/3 6.28)   ; args converted to ints 1 6
-"bcdef"
+(subs "abcdef" 1 3)
+;; => "bc"
+
+(subs "abcdef" 1)
+;; => "bcdef"
+
+(subs "abcdef" 4 6)
+;; => "ef"
+
+(subs "abcdef" 4 7)
+;; => StringIndexOutOfBoundsException String index out of range: 7  java.lang.String.substring (String.java:1907)
+
+;; Fractional indexes aren't meaningful. As a result, args converted to ints by
+;; rounding down, giving the indexes 1 and 6.
+(subs "abcdef" 5/3 6.28)
+;; => "bcdef"
