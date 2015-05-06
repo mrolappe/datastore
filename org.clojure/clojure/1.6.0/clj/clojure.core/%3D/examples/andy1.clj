e780e42a14d37450daeaaca7bbd6ca63c51fdf52
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-06 09:12:22 -0500

    Reformat example for style

--- org.clojure/clojure/1.6.0/clj/clojure.core/%3D/examples/andy1.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/%3D/examples/andy1.clj
@@ -1,18 +1,21 @@
 ;; Same category integer so this should work
-user=> (= 3 3N)
+(= 3 3N)
 ;; => true
 
 ;; Different categories, integer and floating point. This may seem unintuitive,
 ;; however remember that floating point is imprecise. Consequently attempting to
 ;; compare a finite precision floating point result to an integer is not
 ;; generally meaningful and doesn't work quite as expected.
+
 (= 2 2.0)
 ;; => false
 
 ;; Remember collections are equivalent if their members are equivalent..
+
 (= [0 1 2] '(0 1 2))
 ;; => true
 
 ;; But floating point and integer values aren't equivalent.
+
 (= '(0 1 2) '(0 1 2.0))
 ;; => false
