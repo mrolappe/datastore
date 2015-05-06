0da8906c8b1b635f3ffa0766696e5226d80278df
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-06 09:13:22 -0500

    Whitespace

--- org.clojure/clojure/1.6.0/clj/clojure.core/sort/examples/andy2.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/sort/examples/andy2.clj
@@ -2,19 +2,23 @@
 ;; => #'user/x
 
 ;; You can seq an array, the result being the sequence of its elements.
+
 (seq x)
 ;; => (32 9 11)
 
 ;; You can sort it to return the sequence in sorted order
+
 (sort x)
 ;; => (9 11 32)
 
 ;; But sorting also modifies the backing JVM array. Note that (seq x) changed.
+
 (seq x)
 ;; => (9 11 32)
 
 ;; One may avoid this by manually copying the array. Note that this is
 ;; only required for mutable Java arrays, Clojure persistent
 ;; collections will not present this behavior.
+
 (sort (aclone x))
 ;; => (9 11 32)
