bf1137731cb773a2b41445dc782db47c57c64258
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-06 09:11:20 -0500

    Whitespace

--- org.clojure/clojure/1.6.0/clj/clojure.core/%3D%3D/examples/andy1.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/%3D%3D/examples/andy1.clj
@@ -1,13 +1,18 @@
 ;; Some equality exceptions noted by AndyF
 (= 2 2.0)   ; = has different categories integer and floating point
 ;; => false
+
 (== 2 2.0)  ; but == sees same numeric value
 ;; => true
+
 (== 5 5N (float 5.0) (double 5.0) (biginteger 5))
 ;; => true
+
 (== 5 5.0M) ; this is likely a bug
 ;; => false
+
 (== Double/NaN Double/NaN)  ; this is normal
 ;; => false
+
 (== 2 "a")
 ;; => ClassCastException java.lang.String cannot be cast to java.lang.Number  clojure.lang.Numbers.equiv (Numbers.java:206)
