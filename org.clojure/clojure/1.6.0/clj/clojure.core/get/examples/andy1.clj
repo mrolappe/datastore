e1f4ab8701cbc52eb3d279067553d81f92e22181
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-06 09:12:55 -0500

    Whitespace

--- org.clojure/clojure/1.6.0/clj/clojure.core/get/examples/andy1.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/get/examples/andy1.clj
@@ -3,21 +3,25 @@
 ;; => b
 
 ;; 2 is not
+
 (get #{"a" 'b 5 nil} 2)
 ;; => nil
 
 ;; nil is in this set, but one cannot distinguish this result from not
 ;; being in the set.
+
 (get #{"a" 'b 5 nil} nil)
 ;; => nil
 
 ;; The same is true here.
+
 (get #{"a" 'b 5} nil)
 ;; => nil
 
 ;; You may specify a not-found value to help distinguish these cases.
 ;; This works well as long as there is some value you know is not in the
 ;; set.
+
 (get #{"a" 'b 5} nil :not-found)
 ;; => :not-found
 
