fac83f0b2b9325b66e8dce64818fdfa99ddf21b5
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-06 09:11:01 -0500

    Whitespace

--- org.clojure/clojure/1.6.0/clj/clojure.core/contains%3F/examples/andy1.clj
+++ org.clojure/clojure/1.6.0/clj/clojure.core/contains%3F/examples/andy1.clj
@@ -3,13 +3,16 @@
 ;; => true
 
 ;; 2 is not
+
 (contains? #{:a :b 5 nil} 2)
 ;; => false
 
 ;; nil is in this set
+
 (contains? #{:a :b 5 nil} nil)
 ;; => true
 
 ;; but not in this one
+
 (contains? #{:a :b 5} nil)
 ;; => false
