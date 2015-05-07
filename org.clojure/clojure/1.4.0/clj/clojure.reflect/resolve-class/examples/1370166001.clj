10a2e7676aa7b3d78719364d429a213fea1884f7
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-07 12:34:45 -0500

    Formatting

--- org.clojure/clojure/1.4.0/clj/clojure.reflect/resolve-class/examples/1370166001.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.reflect/resolve-class/examples/1370166001.clj
@@ -4,5 +4,5 @@
 (defn class-exists? [c] 
   (resolve-class (.getContextClassLoader (Thread/currentThread)) c))
 
-user=> (class-exists? 'org.joda.time.DateTime) 
-nil
+(class-exists? 'org.joda.time.DateTime) 
+;; => nil
