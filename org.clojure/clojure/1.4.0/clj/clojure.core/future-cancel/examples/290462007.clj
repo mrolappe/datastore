aa8f7622279a1d60fa545ea25261903bf3f7d2d5
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 12:16:47 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/future-cancel/examples/290462007.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/future-cancel/examples/290462007.clj
@@ -1,9 +1,8 @@
+(def f (future (inc 0)))
+;; => #'user/f
 
-user=> (def f (future (inc 0)))
-#'user/f
+(future-cancel f)                           
+;; => false
 
-user=> (future-cancel f)                           
-false
-
-user=> (future-cancelled? f)                       
-false
+(future-cancelled? f)                       
+;; => false
