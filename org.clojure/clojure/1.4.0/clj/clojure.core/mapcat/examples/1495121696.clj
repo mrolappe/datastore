00c4a09956cd8d2f8b17324171b5a28b65cb9e20
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 12:19:19 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/mapcat/examples/1495121696.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/mapcat/examples/1495121696.clj
@@ -3,18 +3,19 @@
 (defn f1
   [n]
   [(- n 1) n (+ n 1)])
+;; => #'user/f1
 
 (f1 1)
-;=> [0 1 2]
+;; => [0 1 2]
 
 ;; Perhaps you'd like to map it onto each item in a collection:
 (map f1 [1 2 3])
-;=> ([0 1 2] [1 2 3] [2 3 4])
+;; => ([0 1 2] [1 2 3] [2 3 4])
 
 ;; But suppose you wanted them all concatenated? You could do this:
 (apply concat (map f1 [1 2 3]))
-;=> (0 1 2 1 2 3 2 3 4)
+;; => (0 1 2 1 2 3 2 3 4)
 
 ;; Or you could get the same thing with `mapcat`:
 (mapcat f1 [1 2 3])
-;=> (0 1 2 1 2 3 2 3 4)
+;; => (0 1 2 1 2 3 2 3 4)
