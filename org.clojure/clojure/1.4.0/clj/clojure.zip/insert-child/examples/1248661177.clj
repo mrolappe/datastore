1a17d9ec0237808bbd8e3d092c6315b3c73cc035
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 01:52:06 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.zip/insert-child/examples/1248661177.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.zip/insert-child/examples/1248661177.clj
@@ -1,18 +1,22 @@
-(use 'clojure.zip) ;;warnings OK
+(require '[clojure.zip :refer :all]) ;; warnings OK
+;; => nil
 
-(def zp (zipper vector? seq (fn [_ c] c) [[1 2 3] [4 [5 6] 7] [8 9]]) )
+(def zp
+  (zipper vector? seq (fn [_ c] c)
+          [[1 2 3] [4 [5 6] 7] [8 9]]))
+;; => #'user/zp
 
 (root (insert-child (-> zp down right) 42))
-=>([1 2 3] (42 4 [5 6] 7) [8 9])
+;; => ([1 2 3] (42 4 [5 6] 7) [8 9])
 
-
-(def zp2 (zipper vector? seq (fn [_ c] c) [74 75])
+(def zp2 (zipper vector? seq (fn [_ c] c) [74 75]))
+;; => #'user/zp2
 
 (root (insert-right (-> zp down right) zp2))
-=>([1 2 3] [4 [5 6] 7] [[74 75] nil] [8 9])
+;; => ([1 2 3] [4 [5 6] 7] [[74 75] nil] [8 9])
 
 (root (insert-left (-> zp down right) zp2))
-=>([1 2 3] [[74 75] nil] [4 [5 6] 7] [8 9])
+;; => ([1 2 3] [[74 75] nil] [4 [5 6] 7] [8 9])
 
 
 
