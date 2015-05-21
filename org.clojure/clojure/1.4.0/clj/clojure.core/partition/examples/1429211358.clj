45f3f485315404ccd91358edb07cbafbaf1c6daf
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 11:47:55 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/partition/examples/1429211358.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/partition/examples/1429211358.clj
@@ -2,14 +2,17 @@
 ;; depends on whether there is a pad or not
 
 ;; without pad
-user> (partition 10 [1 2 3 4])
-()
+(partition 10 [1 2 3 4])
+;; => ()
+
 ;; again, without pad
-user> (partition 10 10 [1 2 3 4])
-()
+(partition 10 10 [1 2 3 4])
+;; => ()
+
 ;; with a pad this time (note: the pad is an empty sequence)
-user> (partition 10 10 nil [1 2 3 4])
-((1 2 3 4))
+(partition 10 10 nil [1 2 3 4])
+;; => ((1 2 3 4))
+
 ;; or, explicit empty sequence instead of nil
-user> (partition 10 10 [] [1 2 3 4])
-((1 2 3 4))
+(partition 10 10 [] [1 2 3 4])
+;; => ((1 2 3 4))
