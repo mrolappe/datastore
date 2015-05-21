4580e166fd03e5881add1b75734258e97e8427bc
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 11:40:23 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/set-error-handler%21/examples/1558934172.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/set-error-handler%21/examples/1558934172.clj
@@ -1,26 +1,28 @@
 (def bond (agent 7))
+;; => #'user/bond
 
 (defn err-handler-fn [ag ex]
   (println "evil error occured: " ex " and we still have value " @ag))
+;; => #'user/err-handler-fn
 
 (set-error-handler! bond err-handler-fn)
 
-;;division by zero:
-
+;; division by zero:
 (send bond (fn [x] (/ x 0)))
-=>evil error occured:  #<ArithmeticException java.lang.ArithmeticException: 
-=>Divide by zero>  and we still have value  7
+;; > evil error occured:  #<ArithmeticException java.lang.ArithmeticException: 
+;; > Divide by zero>  and we still have value  7
 
 (send bond inc)
-=>FAILURE ;;Agent is failed, needs restart, but keeps the last OK value
+;; => FAILURE
+;; Agent is failed, needs restart, but keeps the last OK value
 
 @bond
-=>7
+;; => 7
 
 (restart-agent bond 7) ;; or replace 7 with @ag
 
 (send bond inc)
-=>#<Agent@88d00c6: 7> ;;because of async update
+;; => #<Agent@88d00c6: 7>
 
 @bond
-=>8
+;; => 8
