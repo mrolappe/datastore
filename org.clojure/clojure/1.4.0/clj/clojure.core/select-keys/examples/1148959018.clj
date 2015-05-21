71d070b205f0dc9ca61c39661a021f1100dc5ae1
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 12:19:53 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/select-keys/examples/1148959018.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/select-keys/examples/1148959018.clj
@@ -1,4 +1,5 @@
-user=> (select-keys {:a 1 :b 2} [:a])
-{:a 1}
-user=> (select-keys {:a 1 :b 2} [:a :c])
-{:a 1}
+(select-keys {:a 1 :b 2} [:a])
+;; => {:a 1}
+
+(select-keys {:a 1 :b 2} [:a :c])
+;; => {:a 1}
