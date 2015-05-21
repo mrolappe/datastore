4d3dd4f0d8404800c9bb8387152f3f8d17def54a
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 10:43:07 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.zip/vector-zip/examples/933279738.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.zip/vector-zip/examples/933279738.clj
@@ -1,16 +1,17 @@
-
 (require '[clojure.zip :as zip])
+;; => nil
 
 (defn zip-map [f loc]
-  " Map f over every node of the zipper.
-    The function received has the form (f node-value loc),
-    the node value and its location"
+  "Map f over every node of the zipper. The function received has the
+   form (f node-value loc), the node value and its location"
   (loop [z loc]
     (if (zip/end? z)
-      (zip/root z) ; perhaps you can call zip/seq-zip or zip/vector-zip?
+      (zip/root z)
       (recur (zip/next (zip/edit z f z))))))
+;; => #'user/zip-map
 
-;; Multiply by 100 every node in the tree
-user=> (zip-map (fn [n nx] (if (vector? n) n (* n 100) )) (zip/vector-zip '[5 [10 20 30] [1 2 3] ]))
-;; Be careful! the returned result by zip/root is not a zipper anymore!
-[500 [1000 2000 3000] [100 200 300]]
+;; Multiply by 100 every node in the tree. Be careful! the returned
+;; result by zip/root is not a zipper anymore!
+(zip-map (fn [n nx] (if (vector? n) n (* n 100)))
+         (zip/vector-zip '[5 [10 20 30] [1 2 3]]))
+;; => [500 [1000 2000 3000] [100 200 300]]
