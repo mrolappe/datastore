1c87d8c07ab78de9a8bda973ea5e18d0cd12e331
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:38:47 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/new/examples/1483588723.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/new/examples/1483588723.clj
@@ -1,9 +1,9 @@
 ;; Create a Java ArrayList using the 0 argument constructor
-user=> (def a  (new java.util.ArrayList))
-#'user/a
-user=> (.add a "aaa")
-true
-user=> (.add a "bbb")
-true
-user=> a
-#<ArrayList [aaa, bbb]>
+(def a  (new java.util.ArrayList))
+;; => #'user/a
+(.add a "aaa")
+;; => true
+(.add a "bbb")
+;; => true
+a
+;; => #<ArrayList [aaa, bbb]>
