a2415017114f8c0f484b571ac7631477bb9480e3
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-20 22:12:58 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.string/split/examples/1661432450.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.string/split/examples/1661432450.clj
@@ -1,4 +1,8 @@
-user=> (clojure.string/split "q1w2e3r4t5y6u7i8o9p0" #"\d+")
-["q" "w" "e" "r" "t" "y" "u" "i" "o" "p"]
-user=> (clojure.string/split "q1w2e3r4t5y6u7i8o9p0" #"\d+" 5)
-["q" "w" "e" "r" "t5y6u7i8o9p0"]
+(require '[clojure.string :as s])
+;; => nil
+
+(s/split "q1w2e3r4t5y6u7i8o9p0" #"\d+")
+;; => ["q" "w" "e" "r" "t" "y" "u" "i" "o" "p"]
+
+(s/split "q1w2e3r4t5y6u7i8o9p0" #"\d+" 5)
+;; => ["q" "w" "e" "r" "t5y6u7i8o9p0"]
