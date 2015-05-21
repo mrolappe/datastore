6588b84446565288d19ca0d8814d118b2d832107
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 01:17:01 -0500

    Reformat for whitespace

--- org.clojure/clojure/1.4.0/clj/clojure.set/difference/examples/476044897.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.set/difference/examples/476044897.clj
@@ -1,8 +1,10 @@
 (require '[clojure.set :refer :all])
 ;; => nil
 
-(difference (set (keys {:1 1 :2 2 :3 3})) (set (keys {:1 1 :2 2})))
+(difference (set (keys {:1 1 :2 2 :3 3}))
+            (set (keys {:1 1 :2 2})))
 ;; => #{:3}
 
-(difference (set (keys {:1 1 :2 2})) (set (keys {:1 1 :2 2 :3 3})))
+(difference (set (keys {:1 1 :2 2}))
+            (set (keys {:1 1 :2 2 :3 3})))
 ;; => #{}
