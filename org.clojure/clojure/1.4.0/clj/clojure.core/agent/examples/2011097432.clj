424c7f1a61fe6fb03557f09be0186a207aafd4b4
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:50:49 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/agent/examples/2011097432.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/agent/examples/2011097432.clj
@@ -1,26 +1,35 @@
-; Agents provide shared access to mutable state. They allow non-blocking (asynchronous as opposed to synchronous atoms) and independent change of individual locations (unlike coordinated change of multiple locations through refs).
+;; Agents provide shared access to mutable state. They allow
+;; non-blocking (asynchronous as opposed to synchronous atoms) and
+;; independent change of individual locations (unlike coordinated
+;; change of multiple locations through refs).
 
-; agent creates one:
+;; agent creates one:
 
-user=> (def counter (agent 0))
-#'user/counter
+(def counter (agent 0))
+;; => #'user/counter
 
-; send changes its value:
-user=> (send counter inc)
+;; send changes its value:
+(send counter inc)
+;; => nil
 
-; @ or deref provides a snapshot of the current state:
-user=> @counter
-1
+;; @ or deref provides a snapshot of the current state:
+@counter
+;; => 1
 
-; agents can reference any data structure:
+;; agents can reference any data structure:
 
-user=> (def pulp-fiction (agent {}))
-#'user/pulp-fiction
-user=> (send pulp-fiction assoc :act-one "PROLOGUE")
-user=> @pulp-fiction
-{:act-one "PROLOGUE"}
-user=> (send pulp-fiction assoc :act-two "VINCENT VEGA & MARSELLUS WALLACE'S WIFE")
-user=> @pulp-fiction
-{:act-two "VINCENT VEGA & MARSELLUS WALLACE'S WIFE", :act-one "PROLOGUE"}
+(def pulp-fiction (agent {}))
+;; => #'user/pulp-fiction
+(send pulp-fiction assoc :act-one "PROLOGUE")
+;; => nil
 
-; From http://clojure-examples.appspot.com/clojure.core/agent with permission.
+@pulp-fiction
+;; => {:act-one "PROLOGUE"}
+
+(send pulp-fiction assoc :act-two "VINCENT VEGA & MARSELLUS WALLACE'S WIFE")
+;; => nil
+
+@pulp-fiction
+;; => {:act-two "VINCENT VEGA & MARSELLUS WALLACE'S WIFE", :act-one "PROLOGUE"}
+
+;; From http://clojure-examples.appspot.com/clojure.core/agent with permission.
