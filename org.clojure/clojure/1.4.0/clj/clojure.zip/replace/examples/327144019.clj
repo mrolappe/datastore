6475c7b60871ace31b49186c72920813c7bfe2bb
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 10:41:41 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.zip/replace/examples/327144019.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.zip/replace/examples/327144019.clj
@@ -1,6 +1,9 @@
 (use 'clojure.zip)
+;; => nil
 
-(def vz (vector-zip [1 2 [73 88] 4]))
+(def vz
+  (vector-zip [1 2 [73 88] 4]))
+;; => #'user/vz
 
 (root (replace (-> vz down right right) 3))
-=>[1 2 3 4]
+;; => [1 2 3 4]
