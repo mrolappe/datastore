19df9464fc286ffcbeccc37dcc1e17201df2bfd9
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 11:32:29 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/with-meta/examples/336087950.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/with-meta/examples/336087950.clj
@@ -1,9 +1,11 @@
 ;; the same example above in a simplified way
-user=> (def wm (with-meta [1 2 3] {:my "meta"}))
-#'user/wm
+(def wm
+  (with-meta [1 2 3]
+    {:my "meta"}))
+;; => #'user/wm
 
-user=> wm
-[1 2 3]
+wm
+;; => [1 2 3]
 
-user=> (meta wm)
-{:my "meta"}
+(meta wm)
+;; => {:my "meta"}
