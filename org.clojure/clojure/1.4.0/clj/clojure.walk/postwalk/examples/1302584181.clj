dbccd3f10689f58563bb2ac3ddfddb0f2027418d
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 00:09:21 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.walk/postwalk/examples/1302584181.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.walk/postwalk/examples/1302584181.clj
@@ -1,4 +1,9 @@
-;;example of removing namespaces from all keys in a nested data structure
+(require '[clojure.walk :refer [postwalk]])
+;; => nil
+
+;; example of removing namespaces from all keys in a nested data structure
 (def thing {:page/tags [{:tag/category "lslsls"}]})
+;; => #'user/things
+
 (postwalk #(if(keyword? %)(keyword (name %)) %) thing)
-{:tags [{:category "lslsls"}]}
+;; => {:tags [{:category "lslsls"}]}
