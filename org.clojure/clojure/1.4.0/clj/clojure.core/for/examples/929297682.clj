706f3d2e337af147b94c8e1168e59c2c82dc34b2
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 18:38:51 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/for/examples/929297682.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/for/examples/929297682.clj
@@ -1,5 +1,6 @@
-; Demonstrating difference between :when and :while
-user=> (for [x (range 3) y (range 3) :when (not= x y)] [x y])
-([0 1] [0 2] [1 0] [1 2] [2 0] [2 1])
-user=> (for [x (range 3) y (range 3) :while (not= x y)] [x y])
-([1 0] [2 0] [2 1])
+;; Demonstrating difference between :when and :while
+(for [x (range 3) y (range 3) :when (not= x y)] [x y])
+;; => ([0 1] [0 2] [1 0] [1 2] [2 0] [2 1])
+
+(for [x (range 3) y (range 3) :while (not= x y)] [x y])
+;; => ([1 0] [2 0] [2 1])
