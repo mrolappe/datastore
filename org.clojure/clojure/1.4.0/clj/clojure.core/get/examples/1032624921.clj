83f0cd382336fce23ea227ecf4a07e33608d11f2
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:42:41 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/get/examples/1032624921.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/get/examples/1032624921.clj
@@ -1,10 +1,11 @@
 ;; 'get' is not the only option
-user=> (def my-map {:a 1 :b 2 :c 3})
+(def my-map {:a 1 :b 2 :c 3})
+;; => #'user/my-map
 
 ;; maps act like functions
-user=> (my-map :a)
-1
+(my-map :a)
+;; => 1
 
 ;; even keys act like functions
-user=> (:b my-map)
-2
+(:b my-map)
+;; => 2
