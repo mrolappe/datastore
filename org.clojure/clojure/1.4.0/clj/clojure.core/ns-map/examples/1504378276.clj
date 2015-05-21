d2bcb28915ce67e652676f891c2659c639ad77e6
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:57:08 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/ns-map/examples/1504378276.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/ns-map/examples/1504378276.clj
@@ -1,2 +1,9 @@
 (ns-map 'clojure.core)
-{sorted-map #'clojure.core/sorted-map, read-line #'clojure.core/read-line, re-pattern #'clojure.core/re-pattern, keyword? #'clojure.core/keyword?, ClassVisitor clojure.asm.ClassVisitor, asm-type #'clojure.core/asm-type, val #'clojure.core/val, ...chop...}
+;; => {sorted-map #'clojure.core/sorted-map,
+;;     read-line #'clojure.core/read-line,
+;;     re-pattern #'clojure.core/re-pattern,
+;;     keyword? #'clojure.core/keyword?,
+;;     ClassVisitor clojure.asm.ClassVisitor,
+;;     asm-type #'clojure.core/asm-type,
+;;     val #'clojure.core/val,
+;;     ...}
