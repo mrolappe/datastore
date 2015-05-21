0e74a51f3b2c9fb5143577a61c1dfe170c19e77f
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:43:27 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/get/examples/andy-rounding.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/get/examples/andy-rounding.clj
@@ -1,7 +1,8 @@
 ;; Similar to contains?, `get` has some unusual cases where non-integer
 ;; numbers will be rounded off to integers without any errors.
 
-user=> (get [:a :b :c] 1.7)
-nil     ; on a vector, not found
-user=> (get (int-array [5 6 7]) -0.99)
-5       ; on a Java array, truncate to int, then index is found
+(get [:a :b :c] 1.7)
+;; => nil ; on a vector, not found
+
+(get (int-array [5 6 7]) -0.99)
+;; => 5   ; on a Java array, truncate to int, then index is found
