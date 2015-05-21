dfe57e623d304266e4ea4b6890da67110953ad50
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-20 22:11:30 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.string/replace/examples/1405243999.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.string/replace/examples/1405243999.clj
@@ -1,3 +1,6 @@
+(require '[clojure.string :as s])
+;; => nil
+
 ;; replaces all a's with 1 and all b's with 2
-user=> (clojure.string/replace "a b a" #"a|b" {"a" "1" "b" "2"})
-"1 2 1"
+(s/replace "a b a" #"a|b" {"a" "1" "b" "2"})
+;; => "1 2 1"
