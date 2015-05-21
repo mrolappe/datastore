1efb69499dfb8b1d91a310d3b34256f7270ff035
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 01:22:52 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.set/difference/examples/580058982.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.set/difference/examples/580058982.clj
@@ -1,6 +1,11 @@
-user=> (difference #{1 2 3})
-#{1 2 3}
-user=> (difference #{1 2} #{2 3})
-#{1}
-user=> (difference #{1 2 3} #{1} #{1 4} #{3})
-#{2}
+(require '[clojure.set :refer [difference]])
+;; => nil
+
+(difference #{1 2 3})
+;; => #{1 2 3}
+
+(difference #{1 2} #{2 3})
+;; => #{1}
+
+(difference #{1 2 3} #{1} #{1 4} #{3})
+;; => #{2}
