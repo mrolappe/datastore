79223d8511a5f6415b5a7046240869a7b9336ce1
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:59:07 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/destructure/examples/825701413.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/destructure/examples/825701413.clj
@@ -1,6 +1,9 @@
 ; look ma, no let!
 (map #(intern *ns* (first %) (eval (last %))) (partition 2 (destructure '[[a b] ["a" "b"]])))
-user=> a
-"a"
-user=> b
-"b"
+;; => (#'user/vec__30188 #'user/a #'user/b)
+
+a
+;; => "a"
+
+b
+;; => "b"
