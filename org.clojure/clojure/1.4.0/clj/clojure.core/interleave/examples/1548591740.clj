4ba971b4d787dcb956119daef4155915544c40fe
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:16:29 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/interleave/examples/1548591740.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/interleave/examples/1548591740.clj
@@ -1,7 +1,6 @@
 ;; This example takes a list of keys and a separate list of values and 
 ;; inserts them into a map.
-user=> (apply assoc {} 
-         (interleave [:fruit :color :temp] 
-                     ["grape" "red" "hot"]))
-
-{:temp "hot", :color "red", :fruit "grape"}
+(apply assoc {} 
+       (interleave [:fruit :color :temp] 
+                   ["grape" "red" "hot"]))
+;; => {:temp "hot", :color "red", :fruit "grape"}
