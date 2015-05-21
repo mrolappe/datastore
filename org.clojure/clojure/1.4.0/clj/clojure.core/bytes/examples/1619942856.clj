11ea1e9ecd767ea790301f0b1e286257d19b85d5
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 11:54:48 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/bytes/examples/1619942856.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/bytes/examples/1619942856.clj
@@ -1,14 +1,18 @@
 ;; for fast interop
-user=> (bytes (byte-array (map (comp byte int) "ascii")))
-#<byte[] [B@7a004f62>
-user=> (def the-bytes *1)
-#'user/the-bytes
-user=> (defn get-byte [the-bytes i] (aget the-bytes i))
-#'user/get-byte Reflection warning, NO_SOURCE_PATH:1 - call to aget can't be resolved.
+(bytes (byte-array (map (comp byte int) "ascii")))
+;; => #<byte[] [B@7a004f62>
 
-user=> (defn get-byte [the-bytes i] 
-         (let [the-bytes (bytes the-bytes)] 
-           (aget the-bytes i)))
-#'user/get-byte
-user=> (get-byte the-bytes 0)
-97
+(def the-bytes *1)
+;; => #'user/the-bytes
+
+(defn get-byte [the-bytes i] (aget the-bytes i))
+;; => #'user/get-byte
+;; > Reflection warning, NO_SOURCE_PATH:1 - call to aget can't be resolved.
+
+(defn get-byte [the-bytes i] 
+  (let [the-bytes (bytes the-bytes)] 
+    (aget the-bytes i)))
+;; => #'user/get-byte
+
+(get-byte the-bytes 0)
+;; => 97
