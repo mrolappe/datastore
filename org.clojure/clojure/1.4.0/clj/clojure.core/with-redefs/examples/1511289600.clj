8ee1526c54519140f8b76d7ac3707d3b52167cae
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 18:47:06 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/with-redefs/examples/1511289600.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/with-redefs/examples/1511289600.clj
@@ -1,8 +1,8 @@
 ;; be careful, with-redefs can permanently change a var if applied concurrently:
 
-user> (defn ten [] 10)
-#'user/ten
-user> (doall (pmap #(with-redefs [ten (fn [] %)] (ten)) (range 20 100)))
-...
-user> (ten)
-79
+(defn ten [] 10)
+;; => #'user/ten
+(doall (pmap #(with-redefs [ten (fn [] %)] (ten)) (range 20 100)))
+;; => nil
+(ten)
+;; => 79
