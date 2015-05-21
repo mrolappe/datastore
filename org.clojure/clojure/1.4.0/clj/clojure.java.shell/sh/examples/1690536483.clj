b5b754f1e3ddcaa5edaa3b1ba77784b71794a59f
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 01:43:49 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.java.shell/sh/examples/1690536483.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.java.shell/sh/examples/1690536483.clj
@@ -1,2 +1,5 @@
+(require '[clojure.java.sh :refer [shell]])
+;; => nil
+
 (sh "pwd" :dir "/home/ics/icsdev")
-{:exit 0, :out "/home/ics/icsdev\n", :err ""}
+;; => {:exit 0, :out "/home/ics/icsdev\n", :err ""}
