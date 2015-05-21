49097814c4818d48cc712a211dc4f9acd7485931
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-20 22:09:16 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.string/replace/examples/1327345613.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.string/replace/examples/1327345613.clj
@@ -1,7 +1,11 @@
+(require '[clojure.string :as s])
+;; => nil
+
 ;; To title case
-user=> (clojure.string/replace "hello world" #"\b." #(.toUpperCase %1))
-"Hello World"
+(s/replace "hello world" #"\b." #(.toUpperCase %1))
+;; => "Hello World"
+
 ;; Note that a vector is passed to your replacement function
 ;; when pattern contains capturing groups (see re-groups)
-user=> (clojure.string/replace "hello world" #"\b(.)" #(.toUpperCase (%1 1)))
-"Hello World"
+(s/replace "hello world" #"\b(.)" #(.toUpperCase (%1 1)))
+;; => "Hello World"
