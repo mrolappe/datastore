72f4ec36dcf1f182b4fcad2703e7f188ad27e73b
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 01:29:07 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.set/intersection/examples/1256970642.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.set/intersection/examples/1256970642.clj
@@ -1,8 +1,14 @@
-user=> (intersection #{1})
-#{1}
-user=> (intersection #{1 2} #{2 3})
-#{2}
-user=> (intersection #{1 2} #{2 3} #{3 4})
-#{}
-user=> (intersection #{1 :a} #{:a 3} #{:a})
-#{:a}
+(require '[clojure.set :refer [intersection]])
+;; => nil
+
+(intersection #{1})
+;; => #{1}
+
+(intersection #{1 2} #{2 3})
+;; => #{2}
+
+(intersection #{1 2} #{2 3} #{3 4})
+;; => #{}
+
+(intersection #{1 :a} #{:a 3} #{:a})
+;; => #{:a}
