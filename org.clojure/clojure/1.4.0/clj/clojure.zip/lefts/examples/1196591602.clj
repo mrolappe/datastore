73e3b353b3472d87cdd27bacc41828e80bf87448
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 01:50:50 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.zip/lefts/examples/1196591602.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.zip/lefts/examples/1196591602.clj
@@ -1,4 +1,8 @@
+(require '[clojure.zip :refer [vector-zip down right lefts rights]])
+;; => nil
+
 (def vz (vector-zip [1 2 3 4 5]))
+;; => #'user/vz
 
 ;; root
 ;;  |________
@@ -7,9 +11,9 @@
 
 ;;go down and then right twice, what's to the left of me now?
 (-> vz down right right lefts)
-=>(1 2)
+;; => (1 2)
 
 ;;equivalent:
 (-> vz down right right rights)
-=>(4 5)
+;; => (4 5)
 
