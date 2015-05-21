4564472668ac5412d5708ad1e731c1cadc820cab
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 18:58:06 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/byte-array/examples/285544298.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/byte-array/examples/285544298.clj
@@ -2,17 +2,17 @@
 ;; and demonstrate that you can use it in the standard Java fill function
 ;; note the needed byte coercion in the fill function call
 
-user=> (def bees (byte-array 10))
-#'user/bees
+(def bees (byte-array 10))
+;; => #'user/bees
 
-user=> (for [i (range 10)](aset-byte bees i (* i i)))
-(0 1 4 9 16 25 36 49 64 81)
+(for [i (range 10)](aset-byte bees i (* i i)))
+;; => (0 1 4 9 16 25 36 49 64 81)
 
-user=> (vec bees)
-[0 1 4 9 16 25 36 49 64 81]
+(vec bees)
+;; => [0 1 4 9 16 25 36 49 64 81]
 
-user=> (java.util.Arrays/fill bees (byte 122))
-nil
-user=> (vec bees)
-[122 122 122 122 122 122 122 122 122 122]
-user=>
+(java.util.Arrays/fill bees (byte 122))
+;; => nil
+
+(vec bees)
+;; => [122 122 122 122 122 122 122 122 122 122]
