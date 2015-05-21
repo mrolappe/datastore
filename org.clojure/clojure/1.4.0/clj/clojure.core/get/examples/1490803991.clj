050d03d883ae7aabfec1e8a03f469a1208f5f1a2
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:43:06 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/get/examples/1490803991.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/get/examples/1490803991.clj
@@ -1,9 +1,12 @@
-user=> (get [1 2 3] 1)
-2
-user=> (get [1 2 3] 5)
-nil
-user=> (get {:a 1 :b 2} :b)
-2
-user=> (get {:a 1 :b 2} :z "missing")
-"missing"
+(get [1 2 3] 1)
+;; => 2
+
+(get [1 2 3] 5)
+;; => nil
+
+(get {:a 1 :b 2} :b)
+;; => 2
+
+(get {:a 1 :b 2} :z "missing")
+;; => "missing"
 
