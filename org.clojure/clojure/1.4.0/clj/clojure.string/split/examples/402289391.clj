a70c43492a75cfc08207482532ee78139e613ffd
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-20 22:12:23 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.string/split/examples/402289391.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.string/split/examples/402289391.clj
@@ -1,13 +1,14 @@
 ;; Splits a string on space character and joins 
 ;; the resulting collection with a line feed character
 
-(use '[clojure.string :only (join split)])
+(require '[clojure.string :only [join split]])
+;; => nil
 
-user=> (println
-         (join "\n"
-           (split "The Quick Brown Fox" #"\s")))
-The
-Quick
-Brown
-Fox
-nil
+(println
+ (join "\n"
+       (split "The Quick Brown Fox" #"\s")))
+;; > The
+;; > Quick
+;; > Brown
+;; > Fox
+;; => nil
