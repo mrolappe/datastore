be2721621da26c07e6181573856071637d575a70
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 17:20:53 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/interleave/examples/989817802.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/interleave/examples/989817802.clj
@@ -1,5 +1,7 @@
 (def s1 [[:000-00-0000 "TYPE 1" "JACKSON" "FRED"]
          [:000-00-0001 "TYPE 2" "SIMPSON" "HOMER"]
          [:000-00-0002 "TYPE 4" "SMITH" "SUSAN"]])
+;; => #'user/s1
 
 (interleave (map #(nth % 0 nil) s1) (map #(nth % 1 nil) s1))
+;; => (:000-00-0000 "TYPE 1" :000-00-0001 "TYPE 2" :000-00-0002 "TYPE 4")
