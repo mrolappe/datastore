cbee1f3549420b4c92363a0b7989b74f262d257c
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 19:19:48 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/read-string/examples/1407205074.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/read-string/examples/1407205074.clj
@@ -1,8 +1,8 @@
 ;; you can think of read-string as the inverse of pr-str
 ;; turn string into symbols
-user=> (read-string "(a b foo :bar)")
-(a b foo :bar)
+(read-string "(a b foo :bar)")
+;; => (a b foo :bar)
 
 ;;turn symbols into a string
-user=> (pr-str '(a b foo :bar))
-"(a b foo :bar)"
+(pr-str '(a b foo :bar))
+;; => "(a b foo :bar)"
