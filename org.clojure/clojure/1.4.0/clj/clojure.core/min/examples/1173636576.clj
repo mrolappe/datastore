a69d80f93a6ceb35b1fca5c778d537c48dcfbec6
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 19:15:23 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/min/examples/1173636576.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/min/examples/1173636576.clj
@@ -1,5 +1,6 @@
 ;; If elements are already in a sequence, use apply
-user=> (apply min [1 2 3 4 3])
-1
-user=> (apply min '(4 3 5 6 2))
-2
+(apply min [1 2 3 4 3])
+;; => 1
+
+(apply min '(4 3 5 6 2))
+;; => 2
