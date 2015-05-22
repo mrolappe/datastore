b1f5eca1ed8a99e5455f9a2c8ffaad457954cd7c
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-21 19:16:24 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/shutdown-agents/examples/2054918613.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/shutdown-agents/examples/2054918613.clj
@@ -1,43 +1,43 @@
 ;; Creating an agent
-user> (def a (agent 1))
-#'user/a
+(def a (agent 1))
+;; => #'user/a
 
 ;; Create a function that can handle an agent
 
-user> (defn agent-action [a]
-	33)
-#'user/agent-action
+(defn agent-action [a]
+  33)
+;; => #'user/agent-action
 
 ;; The agent will become 33
-user> (send-off a agent-action)
-#<Agent@dde4f2: 33>
+(send-off a agent-action)
+;; => #<Agent@dde4f2: 33>
 
-user> @a
-33
+@a
+;; => 33
 ;; Create another agent before shutdown
-user> (def c (agent 3))
-#'user/c
+(def c (agent 3))
+;; => #'user/c
 
 ;; Shutdown agents is called
-user> (shutdown-agents)
-nil
+(shutdown-agents)
+;; => nil
 
 ;; Attempt to turn c into 33
-user> (send c agent-action)
-#<Agent@b162fa: 3>
+(send c agent-action)
+;; => #<Agent@b162fa: 3>
 
 ;; The result is that it is still the same value it was initialized with
-user> @c
-3
+@c
+;; => 3
 
 ;; Agent created after shutdown
-user> (def d (agent 4))
-#'user/d
+(def d (agent 4))
+;; => #'user/d
 
 ;; Try sending it
-user> (send d agent-action)
-#<Agent@356519: 4>
+(send d agent-action)
+;; => #<Agent@356519: 4>
 
 ;; Same thing, there are no threads to process the agents
-user> @d
-4
+@d
+;; => 4
