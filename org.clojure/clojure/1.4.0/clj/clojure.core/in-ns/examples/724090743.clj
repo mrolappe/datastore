bfe1e59075f1dcc9ea693e26b7af271cf490e7ff
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 11:14:27 -0700

    Reformat for stlye

--- org.clojure/clojure/1.4.0/clj/clojure.core/in-ns/examples/724090743.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/in-ns/examples/724090743.clj
@@ -1,11 +1,16 @@
 ;; The "in-ns" function works almost the same as "ns", but does not load clojure.core 
 
-user=> (in-ns 'my-namespace)
+(in-ns 'my-namespace)
 #<Namespace my-namespace>
 
-;; the function clojure.core/inc won't just work
-my-namespace=> (inc 1)
-java.lang.Exception: Unable to resolve symbol: inc in this context (NO_SOURCE_FILE:15)
+;; The function clojure.core/inc won't just work becayse
+;; the "my-namespace" ns is empty and doesn't alias in all the defs
+;; from clojure.core. As a result if we just say inc...
+(inc 1)
+;; => java.lang.Exception: Unable to resolve symbol: inc in this context (NO_SOURCE_FILE:15)
+;; we can't find it.
 
-my-namespace=> (clojure.core/inc 1)
-2
+;; We can however use the fully qualified name to refer to inc despite
+;; not having aliased it in.
+(clojure.core/inc 1)
+;; => 2
