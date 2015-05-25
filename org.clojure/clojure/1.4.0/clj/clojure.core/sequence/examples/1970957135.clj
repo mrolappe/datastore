60bc8358b5c5b6e552aac32e5227d840299d1800
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:18:29 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/sequence/examples/1970957135.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/sequence/examples/1970957135.clj
@@ -1,6 +1,4 @@
-user> (sequence [1 2 3])
-(1 2 3)
-user> 
-(1 2 3)
-user> (class (sequence '(1 2 3)))
-clojure.lang.PersistentList
+(sequence [1 2 3])
+;; => (1 2 3)
+(class (sequence '(1 2 3)))
+;; => clojure.lang.PersistentList
