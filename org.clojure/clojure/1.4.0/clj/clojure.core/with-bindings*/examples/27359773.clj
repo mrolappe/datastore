223ffc56fbac2ba3e7a7f2e9087747aad0f4fcc4
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:53:18 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/with-bindings*/examples/27359773.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/with-bindings*/examples/27359773.clj
@@ -1,3 +1,3 @@
-user=> (let [f (fn [] *warn-on-reflection*)]
-         (with-bindings* {#'*warn-on-reflection* true} f))
-true
+(let [f (fn [] *warn-on-reflection*)]
+  (with-bindings* {#'*warn-on-reflection* true} f))
+;; => true
