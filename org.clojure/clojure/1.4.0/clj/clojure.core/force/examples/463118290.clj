af947395554573decc86f6b1d60ad546a3a1b6f7
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:30:32 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/force/examples/463118290.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/force/examples/463118290.clj
@@ -8,33 +8,33 @@
 
 (defn tarai-d [x y z]
   (if (<= (force x) (force y))
-      (force y)
-      (recur (delay (tarai-d (- (force x) 1) y z))
-               (delay (tarai-d (- (force y) 1) z x))
-               (delay (tarai-d (- (force z) 1) x y)))))
+    (force y)
+    (recur (delay (tarai-d (- (force x) 1) y z))
+           (delay (tarai-d (- (force y) 1) z x))
+           (delay (tarai-d (- (force z) 1) x y)))))
 
-user> (dotimes [_ 10] (time (tarai (fn [] 192) (fn [] 96) (fn [] 0))))
-"Elapsed time: 139.660729 msecs"
-"Elapsed time: 132.493587 msecs"
-"Elapsed time: 135.867772 msecs"
-"Elapsed time: 132.924774 msecs"
-"Elapsed time: 137.491084 msecs"
-"Elapsed time: 134.72752 msecs"
-"Elapsed time: 132.969652 msecs"
-"Elapsed time: 135.795754 msecs"
-"Elapsed time: 134.261724 msecs"
-"Elapsed time: 138.059968 msecs"
+(dotimes [_ 10] (time (tarai (fn [] 192) (fn [] 96) (fn [] 0))))
+;; > "Elapsed time: 139.660729 msecs"
+;; > "Elapsed time: 132.493587 msecs"
+;; > "Elapsed time: 135.867772 msecs"
+;; > "Elapsed time: 132.924774 msecs"
+;; > "Elapsed time: 137.491084 msecs"
+;; > "Elapsed time: 134.72752 msecs"
+;; > "Elapsed time: 132.969652 msecs"
+;; > "Elapsed time: 135.795754 msecs"
+;; > "Elapsed time: 134.261724 msecs"
+;; > "Elapsed time: 138.059968 msecs"
+;; => nil
 
-nil
-user> (dotimes [_ 10 ]  (time (tarai-d 192 96 0)))
-"Elapsed time: 3.181795 msecs"
-"Elapsed time: 2.960096 msecs"
-"Elapsed time: 3.000855 msecs"
-"Elapsed time: 3.140536 msecs"
-"Elapsed time: 3.658821 msecs"
-"Elapsed time: 3.319659 msecs"
-"Elapsed time: 2.9182 msecs"
-"Elapsed time: 3.125442 msecs"
-"Elapsed time: 2.944342 msecs"
-"Elapsed time: 2.951613 msecs"
-nil
+(dotimes [_ 10 ]  (time (tarai-d 192 96 0)))
+;; > "Elapsed time: 3.181795 msecs"
+;; > "Elapsed time: 2.960096 msecs"
+;; > "Elapsed time: 3.000855 msecs"
+;; > "Elapsed time: 3.140536 msecs"
+;; > "Elapsed time: 3.658821 msecs"
+;; > "Elapsed time: 3.319659 msecs"
+;; > "Elapsed time: 2.9182 msecs"
+;; > "Elapsed time: 3.125442 msecs"
+;; > "Elapsed time: 2.944342 msecs"
+;; > "Elapsed time: 2.951613 msecs"
+;; => nil
