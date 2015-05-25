dcdee1d40f540bfbfa46e4e4317bcd95fb240187
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 10:58:24 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/aset-long/examples/1544063714.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/aset-long/examples/1544063714.clj
@@ -1,11 +1,12 @@
 ;; create an array of 10 longs and set one of the values to 31415
+(def ls (long-array 10))
+;; => #'user/ls
 
-user=> (def ls (long-array 10))
-#'user/ls
-user=> (vec ls)
-[0 0 0 0 0 0 0 0 0 0]
-user=> (aset-long ls 3 31415)
-31415
-user=> (vec ls)
-[0 0 0 31415 0 0 0 0 0 0]
-user=>
+(vec ls)
+;; => [0 0 0 0 0 0 0 0 0 0]
+
+(aset-long ls 3 31415)
+;; => 31415
+
+(vec ls)
+;; => [0 0 0 31415 0 0 0 0 0 0]
