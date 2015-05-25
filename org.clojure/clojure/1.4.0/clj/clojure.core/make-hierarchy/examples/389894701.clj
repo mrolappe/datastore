e4dac795466eea8ef7771de0339255d0ab6cd07d
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:57:00 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/make-hierarchy/examples/389894701.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/make-hierarchy/examples/389894701.clj
@@ -3,18 +3,22 @@
 ;; note that the first ancestors call returns a nil since that type does not 
 ;; exist in the global hierarchy
 
-user=> (def h (make-hierarchy))
-#'user/h
-user=> (def h (derive h ::rect ::shape))
-#'user/h
-user=> (def h (derive h ::square ::rect))
-#'user/h
-user=> h
-{:parents {:user/square #{:user/rect}, :user/rect #{:user/shape}}, :ancestors {:
-user/square #{:user/shape :user/rect}, :user/rect #{:user/shape}}, :descendants
-{:user/rect #{:user/square}, :user/shape #{:user/square :user/rect}}}
-user=> (ancestors ::square)
-nil
-user=> (ancestors h ::square)
-#{:user/shape :user/rect}
-user=>
+(def h (make-hierarchy))
+;; => #'user/h
+
+(def h (derive h ::rect ::shape))
+;; => #'user/h
+
+(def h (derive h ::square ::rect))
+;; => #'user/h
+
+;; => h
+;; => {:parents {:user/square #{:user/rect}, :user/rect #{:user/shape}},
+;;     :ancestors {:user/square #{:user/shape :user/rect}, :user/rect #{:user/shape}},
+;;     :descendants {:user/rect #{:user/square}, :user/shape #{:user/square :user/rect}}}
+
+(ancestors ::square)
+;; => nil
+
+(ancestors h ::square)
+;; => #{:user/shape :user/rect}
