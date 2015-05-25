19fbf69ed435d89ce543ad725f748f660ef840cc
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 03:16:58 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/alength/examples/326847213.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/alength/examples/326847213.clj
@@ -1,5 +1,5 @@
-user=> (def my-array (into-array Integer/TYPE [1 2 3]))
-#'user/my-array
+(def my-array (into-array Integer/TYPE [1 2 3]))
+;; => #'user/my-array
 
-user=> (alength my-array)
-3
+(alength my-array)
+;; => 3
