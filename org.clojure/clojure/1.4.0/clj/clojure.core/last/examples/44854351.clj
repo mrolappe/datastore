9227704b6a693b02b4372d096a951bcc737cc298
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 10:52:50 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/last/examples/44854351.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/last/examples/44854351.clj
@@ -1,8 +1,11 @@
-user=> (last [1 2 3 4 5])
-5
-user=> (last ["a" "b" "c" "d" "e"])
-"e"
-user=> (last {:one 1 :two 2 :three 3})
-[:three 3]
-user=> (last [])
-nil
+(last [1 2 3 4 5])
+;; => 5
+
+(last ["a" "b" "c" "d" "e"])
+;; => "e"
+
+(last {:one 1 :two 2 :three 3})
+;; => [:three 3]
+
+(last [])
+;; => nil
