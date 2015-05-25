de93b0732b1c4fb9dd24ba90d6cf211155b4d4b5
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:52:06 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/replicate/examples/25692419.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/replicate/examples/25692419.clj
@@ -1,5 +1,7 @@
-user=> (apply str (replicate 7 \space))
-       ; 7 spaces
+(apply str (replicate 7 \space))
+;; => "       "
+;; 7 spaces
 
-user=> (replicate 7 (rand-int 10))
-(3 3 3 3 3 3 3) ; the same number
+(replicate 7 (rand-int 10))
+;; => (3 3 3 3 3 3 3)
+;; the same number
