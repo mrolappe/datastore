ab392af33cb808094ff35f3329fa5fa1a5958d23
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 11:26:20 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/reduce/examples/1338560375.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/reduce/examples/1338560375.clj
@@ -1,3 +1,7 @@
-;; converting a vector to a set
-(reduce conj #{} [:a :b :c])
+;; converting a vector to a set.
+(into #{} [:a :b :c])
 ;; => #{:a :c :b}
+;; This is the same as writing (reduce conj #{} [:a :b :c]), however
+;; it will perform better in the general case because where (reduce
+;; conj) will construct intermediary persistent datastructures, into
+;; will take advantage of transient operations if possible.
