e16e6356f4fd0feadef4572f6854dcd34ed8234e
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:46:15 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/or/examples/100096546.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/or/examples/100096546.clj
@@ -1,26 +1,26 @@
-user> (or true false false)
-true
+(or true false false)
+;; => true
 
-user> (or true true true)
-true
+(or true true true)
+;; => true
 
-user> (or false false false)
-false
+(or false false false)
+;; => false
 
-user> (or nil nil)
-nil
+(or nil nil)
+;; => nil
 
-user> (or false nil)
-nil
+(or false nil)
+;; => nil
 
-user> (or true nil)
-true
+(or true nil)
+;; => true
 
 ;; or doesn't evaluate if the first value is true
-user> (or true (println "foo"))
-true
+(or true (println "foo"))
+;; => true
 
 ;; order matters
-user> (or (println "foo") true)
-foo
-true
+(or (println "foo") true)
+;; > foo
+;; => true
