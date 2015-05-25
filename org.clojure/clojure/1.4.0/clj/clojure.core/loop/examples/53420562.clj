3538e27e0cb830bd579718f412e4ef220182a78e
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 11:32:05 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/loop/examples/53420562.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/loop/examples/53420562.clj
@@ -1,23 +1,23 @@
 (defn find-needle [needle haystack]
-  ;loop binds initial values once,
-  ;then binds values from each recursion call
+  ;; loop binds initial values once,
+  ;; then binds values from each recursion call
   (loop [needle needle
          maybe-here haystack
          not-here '()]
 
     (let [needle? (first maybe-here)]
 
-      ;test for return or recur
+      ;; test for return or recur
       (if (or (= (str needle?) (str needle))
               (empty? maybe-here))
 
-        ;return results
+        ;; return results
         [needle? maybe-here not-here]
 
-        ;recur calls loop with new values
+        ;; recur calls loop with new values
         (recur needle
                (rest maybe-here)
                (concat not-here (list (first maybe-here))))))))
 
-user=>(find-needle "|" "hay|stack")
-[\| (\| \s \t \a \c \k) (\h \a \y)]
+(find-needle "|" "hay|stack")
+;; => [\| (\| \s \t \a \c \k) (\h \a \y)]
