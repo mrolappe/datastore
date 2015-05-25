3dc7aa733fbfd66ef264467d69f0bfbd95444715
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:56:01 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/aset-float/examples/689057448.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/aset-float/examples/689057448.clj
@@ -1,11 +1,13 @@
 ;; create an array of 10 floats and set one of the values to 3.1415
 
-user=> (def fs (float-array 10))
-#'user/fs
-user=> (vec fs)
-[0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
-user=> (aset-float fs 3 3.1415)
-3.1415
-user=> (vec fs)
-[0.0 0.0 0.0 3.1415 0.0 0.0 0.0 0.0 0.0 0.0]
-user=>
+(def fs (float-array 10))
+;; => #'user/fs
+
+(vec fs)
+;; => [0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
+
+(aset-float fs 3 3.1415)
+;; => 3.1415
+
+(vec fs)
+;; => [0.0 0.0 0.0 3.1415 0.0 0.0 0.0 0.0 0.0 0.0]
