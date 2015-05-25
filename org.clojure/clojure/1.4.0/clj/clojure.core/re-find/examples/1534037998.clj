cda0ea1b4400acc64595c5c788f09d05bb93afc1
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 11:33:30 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/re-find/examples/1534037998.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/re-find/examples/1534037998.clj
@@ -1,5 +1,7 @@
 ;; If your input has line delimiters you can switch on multiline with (?m)
 
-user=> (def testcase "Line 1\nLine 2\nTarget Line\nLine 4\nNot a target line")
-user=>(re-find #"(?im)^target.*$" testcase)
-"Target Line"
+(def testcase "Line 1\nLine 2\nTarget Line\nLine 4\nNot a target line")
+;; => #'user/testcase
+
+(re-find #"(?im)^target.*$" testcase)
+;; => "Target Line"
