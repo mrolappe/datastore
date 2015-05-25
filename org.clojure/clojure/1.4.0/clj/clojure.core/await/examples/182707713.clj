c9f0b8c17a51026e1ce95eb1b7eea4eeb807dbf3
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 03:16:40 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/await/examples/182707713.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/await/examples/182707713.clj
@@ -1,9 +1,10 @@
 (def *agnt* (agent {}))
+;; => #'user/*agnt*
 
-user=> (send-off *agnt* (fn [state] 
-                          (Thread/sleep 10000)
-                          (assoc state :done true)))
-#<Agent@5db18235: {}>
+(send-off *agnt* (fn [state] 
+                   (Thread/sleep 10000)
+                   (assoc state :done true)))
+;; => #<Agent@5db18235: {}>
 
-user=> (await *agnt*) ; blocks till the agent action is finished
-nil
+(await *agnt*) ; blocks till the agent action is finished
+;; => nil
