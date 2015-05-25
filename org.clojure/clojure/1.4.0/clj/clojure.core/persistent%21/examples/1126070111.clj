5774fab86560ec5a1ba30ca6240b0d0c14069990
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-25 02:45:40 -0500

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/persistent%21/examples/1126070111.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/persistent%21/examples/1126070111.clj
@@ -1,12 +1,17 @@
-user> (def foo (transient [1 2 3]))
-#'user/foo
-user> foo
-#<TransientVector clojure.lang.PersistentVector$TransientVector@12c9b4d1>
-user> (persistent! foo)
-[1 2 3]
-user> foo
-#<TransientVector clojure.lang.PersistentVector$TransientVector@12c9b4d1>
-user> (conj! foo 4)
-â†’ ERROR:Transient used after persistent! call
-user> (persistent! foo)
-â†’ ERROR: Transient used after persistent! call
+(def foo (transient [1 2 3]))
+;; => #'user/foo
+
+foo
+;; => #<TransientVector clojure.lang.PersistentVector$TransientVector@12c9b4d1>
+
+(persistent! foo)
+;; => [1 2 3]
+
+foo
+;; => #<TransientVector clojure.lang.PersistentVector$TransientVector@12c9b4d1>
+
+(conj! foo 4)
+;; => ERROR:Transient used after persistent! call
+
+(persistent! foo)
+;; => ERROR: Transient used after persistent! call
