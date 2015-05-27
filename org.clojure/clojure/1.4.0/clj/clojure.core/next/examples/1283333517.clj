9aa732700e572ec54a438442324af25f31b5b114
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:15:38 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/next/examples/1283333517.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/next/examples/1283333517.clj
@@ -1,8 +1,8 @@
-user=> (next '(:alpha :bravo :charlie))
-(:bravo :charlie)
+(next '(:alpha :bravo :charlie))
+;; => (:bravo :charlie)
 
-user=> (next (next '(:one :two :three)))
-(:three)
+(next (next '(:one :two :three)))
+;; => (:three)
 
-user=> (next (next (next '(:one :two :three))))
-nil
+(next (next (next '(:one :two :three))))
+;; => nil
