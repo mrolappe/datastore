f48888d732210e65ebfcec2c0cef3903fa4168a6
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:22:30 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/repeat/examples/506951510.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/repeat/examples/506951510.clj
@@ -1,9 +1,9 @@
-user=> (take 5 (repeat "x"))
-("x" "x" "x" "x" "x")
+(take 5 (repeat "x"))
+;; => ("x" "x" "x" "x" "x")
 
 ;; which is the same as:
-user=> (repeat 5 "x")
-("x" "x" "x" "x" "x")
+(repeat 5 "x")
+;; => ("x" "x" "x" "x" "x")
 
 ;; It should be noted that repeat simply repeats the value n number of times.
 ;; If you wish to execute a function to calculate the value each time you 
