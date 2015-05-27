9907df4508f29dfd914c0dca2c27862811bcdfe2
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:23:41 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/into/examples/481152301.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/into/examples/481152301.clj
@@ -1,4 +1,4 @@
-; Items are conj'ed one at a time, which puts them at the head of 
-; the destination list
-user=> (into () '(1 2 3))
-(3 2 1)
+;; Items are conj'ed one at a time, which puts them at the head of 
+                                        ; the destination list
+(into () '(1 2 3))
+;; => (3 2 1)
