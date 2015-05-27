1b3dd7f5b56dbc3adc56e7a94b581fbe58093cc5
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:28:08 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/seq/examples/1867777931.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/seq/examples/1867777931.clj
@@ -1,3 +1,3 @@
-;(seq x) is the recommended idiom for testing if a collection is not empty
-user=> (every? seq ["1" [1] '(1) {:1 1} #{1}])
-true
+;; (seq x) is the recommended idiom for testing if a collection is not empty
+(every? seq ["1" [1] '(1) {:1 1} #{1}])
+;; => true
