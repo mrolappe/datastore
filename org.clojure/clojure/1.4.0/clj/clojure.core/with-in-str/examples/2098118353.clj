fe781288afe1f4b1e4c066be9cdec01bda1baf0f
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:10:33 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/with-in-str/examples/2098118353.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/with-in-str/examples/2098118353.clj
@@ -3,13 +3,13 @@
   (println question)
   (read-line))
 
-user=> (prompt "How old are you?")
-How old are you?
-34                   ; <== This is what you enter
-"34"                 ; <== This is returned by the function
+(prompt "How old are you?")
+;; > How old are you?
+;; < 34                   ; <== This is what you enter
+;; => "34"                 ; <== This is returned by the function
 
 ;; You can now simulate entering your age at the prompt by using with-in-str
 
-user=> (with-in-str "34" (prompt "How old are you?"))
-How old are you?
-"34"                 ; <== The function now returns immediately 
+(with-in-str "34" (prompt "How old are you?"))
+;; > How old are you?
+;; => "34"                 ; <== The function now returns immediately 
