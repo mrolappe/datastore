581207ec067a2b5e5c99822285f7dae7989a091d
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:42:25 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/require/examples/879251722.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/require/examples/879251722.clj
@@ -1,13 +1,13 @@
 ;; alias clojure.java.io as io
-user=> (require '[clojure.java.io :as io])
-nil
+(require '[clojure.java.io :as io])
+;; => nil
 
-user=> (io/file "Filename")
-#<File Filename>
+(io/file "Filename")
+;; => #<File Filename>
 
 ;; alias clojure.java.io as io using prefixes
-user=> (require '(clojure.java [io :as io2])
-nil
+(require '(clojure.java [io :as io2]))
+;; => nil
 
-user=> (io2/file "Filename")
-#<File Filename>
+(io2/file "Filename")
+;; => #<File Filename>
