b8e44fd8b2527328230c44947aef45cc62996838
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:41:23 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/compare/examples/andy-exception.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/compare/examples/andy-exception.clj
@@ -5,12 +5,14 @@
 ;; comparator if you wish to sort such values.  See [Comparators in
 ;; Clojure][Comparators] for examples of comparators that can do this.
 
-user=> (sort [5 "a"])
-ClassCastException java.lang.Long cannot be cast to java.lang.String  java.lang.String.compareTo (String.java:108)
-user=> (sort [:foo 'bar])
-ClassCastException clojure.lang.Keyword cannot be cast to clojure.lang.Symbol  clojure.lang.Symbol.compareTo (Symbol.java:106)
+(sort [5 "a"])
+;; => ClassCastException java.lang.Long cannot be cast to java.lang.String  java.lang.String.compareTo (String.java:108)
 
-user=> (sort [#{1 2} {2 4}])
-ClassCastException clojure.lang.PersistentArrayMap cannot be cast to java.lang.Comparable  clojure.lang.Util.compare (Util.java:153)
-user=> (sort [{:a 1 :b 3} {:c -2 :d 4}])
-ClassCastException clojure.lang.PersistentArrayMap cannot be cast to java.lang.Comparable  clojure.lang.Util.compare (Util.java:153)
+(sort [:foo 'bar])
+;; => ClassCastException clojure.lang.Keyword cannot be cast to clojure.lang.Symbol  clojure.lang.Symbol.compareTo (Symbol.java:106)
+
+(sort [#{1 2} {2 4}])
+;; => ClassCastException clojure.lang.PersistentArrayMap cannot be cast to java.lang.Comparable  clojure.lang.Util.compare (Util.java:153)
+
+(sort [{:a 1 :b 3} {:c -2 :d 4}])
+;; => ClassCastException clojure.lang.PersistentArrayMap cannot be cast to java.lang.Comparable  clojure.lang.Util.compare (Util.java:153)
