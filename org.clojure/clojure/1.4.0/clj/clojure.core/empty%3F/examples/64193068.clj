5d875da631fed16caa5888a1402402b445d1a7d3
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:16:40 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/empty%3F/examples/64193068.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/empty%3F/examples/64193068.clj
@@ -1,6 +1,6 @@
-user=> (every? empty? ["" [] () '() {} #{} nil])
-true
+(every? empty? ["" [] () '() {} #{} nil])
+;; => true
 
-;example of recommended idiom for testing if not empty
-user=> (every? seq ["1" [1] '(1) {:1 1} #{1}])
-true
+;; example of recommended idiom for testing if not empty
+(every? seq ["1" [1] '(1) {:1 1} #{1}])
+;; => true
