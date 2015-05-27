7b1d286d47a3d848d79b89cc4141a4b41d92926a
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-05-26 21:11:46 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/nnext/examples/1729894702.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/nnext/examples/1729894702.clj
@@ -1,19 +1,18 @@
-user=> (nnext '(1 2 3))
-(3)
+(nnext '(1 2 3))
+;; => (3)
 
 
-user=> (nnext [])
-nil 
+(nnext [])
+;; => nil
 
 
-user=> (nnext ['(a b c) '(b a c) '(c b a) '(a c b)])
-((c b a) (a c b)) 
+(nnext ['(a b c) '(b a c) '(c b a) '(a c b)])
+;; => ((c b a) (a c b))
 
 
-user=> (nnext {:a 1, :b 2, :c 3, :d 4})
-([:c 3] [:d 4]) 
+(nnext {:a 1, :b 2, :c 3, :d 4})
+;; => ([:c 3] [:d 4])
 
 
-user=> (nnext #{:a :b :c})
-(:c)
-
+(nnext #{:a :b :c})
+;; => (:c)
