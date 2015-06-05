09ebeea2a47c5b4a955c3fc7ce9f03a9e999ce22
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:13:10 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/unchecked-add/examples/1906059119.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/unchecked-add/examples/1906059119.clj
@@ -2,26 +2,26 @@
 ;; F is a function, not an operator.
 ;; overflow very easily as shown below.
 
-user=> (unchecked-add Integer/MAX_VALUE 0)
-2147483647
+(unchecked-add Integer/MAX_VALUE 0)
+;; => 2147483647
 
-user=> (unchecked-add Integer/MAX_VALUE 1)
--2147483648
+(unchecked-add Integer/MAX_VALUE 1)
+;; => -2147483648
 
-user=> (unchecked-add Integer/MAX_VALUE Integer/MAX_VALUE)
--2
+(unchecked-add Integer/MAX_VALUE Integer/MAX_VALUE)
+;; => -2
 
-user=> (unchecked-add Integer/MAX_VALUE Long/MAX_VALUE)
-java.lang.IllegalArgumentException: No matching method found: unchecked_add (NO_SOURCE_FILE:0)
+(unchecked-add Integer/MAX_VALUE Long/MAX_VALUE)
+;; => java.lang.IllegalArgumentException: No matching method found: unchecked_add (NO_SOURCE_FILE:0)
 
-user=> (unchecked-add Integer/MAX_VALUE Long/MAX_VALUE)
-java.lang.IllegalArgumentException: No matching method found: unchecked_add (NO_SOURCE_FILE:0)
+(unchecked-add Integer/MAX_VALUE Long/MAX_VALUE)
+;; => java.lang.IllegalArgumentException: No matching method found: unchecked_add (NO_SOURCE_FILE:0)
 
-user=> (unchecked-add Long/MAX_VALUE Long/MAX_VALUE)
--2
+(unchecked-add Long/MAX_VALUE Long/MAX_VALUE)
+;; => -2
 
-user=> (unchecked-add 5 Long/MAX_VALUE)
-java.lang.IllegalArgumentException: No matching method found: unchecked_add (NO_SOURCE_FILE:0)
+(unchecked-add 5 Long/MAX_VALUE)
+;; => java.lang.IllegalArgumentException: No matching method found: unchecked_add (NO_SOURCE_FILE:0)
 
-user=> (unchecked-add 5555555555 Long/MAX_VALUE)
--9223372031299220254
+(unchecked-add 5555555555 Long/MAX_VALUE)
+;; => -9223372031299220254
