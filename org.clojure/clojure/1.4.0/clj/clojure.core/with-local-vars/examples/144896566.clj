aba46d9630547cbb2d344d4abf17b42332dc8f73
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:16:31 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/with-local-vars/examples/144896566.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/with-local-vars/examples/144896566.clj
@@ -2,12 +2,13 @@
 ;; where you really want to.  factorial isn't a case where it helps, but
 ;; it is short and familiar.  Note that (var-get acc) can be abbreviated
 ;; as @acc
-user=> (defn factorial [x]
-         (with-local-vars [acc 1, cnt x]
-           (while (> @cnt 0)
-             (var-set acc (* @acc @cnt))
-             (var-set cnt (dec @cnt)))
-           @acc))
-#'user/factorial
-user=> (factorial 7)
-5040
+(defn factorial [x]
+  (with-local-vars [acc 1, cnt x]
+    (while (> @cnt 0)
+      (var-set acc (* @acc @cnt))
+      (var-set cnt (dec @cnt)))
+    @acc))
+;; => #'user/factorial
+
+(factorial 7)
+;; => 5040
