3dfd1e710e7a83b90c072ce1b4bda492346c959a
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 14:26:53 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/aclone/examples/940469112.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/aclone/examples/940469112.clj
@@ -2,13 +2,17 @@
 ;; note that when you modify b, a remains the same
 ;; showing that b is not just a reference to a
 
-user=> (def a (int-array [1 2 3 4]))
-#'user/a
-user=> (def b (aclone a))
-#'user/b
-user=> (aset b 0 23)
-23
-user=> (vec b)
-[23 2 3 4]
-user=> (vec a)
-[1 2 3 4]
+(def a (int-array [1 2 3 4]))
+;; => #'user/a
+
+(def b (aclone a))
+;; => #'user/b
+
+(aset b 0 23)
+;; => 23
+
+(vec b)
+;; => [23 2 3 4]
+
+(vec a)
+;; => [1 2 3 4]
