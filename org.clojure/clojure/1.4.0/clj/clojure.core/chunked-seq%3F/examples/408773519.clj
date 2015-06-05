5ddd9d920f68caf12095bf3593289576c880789b
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:28:10 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/chunked-seq%3F/examples/408773519.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/chunked-seq%3F/examples/408773519.clj
@@ -1,11 +1,11 @@
-user=> (chunked-seq? (range 1000))
-false
+(chunked-seq? (range 1000))
+;; => false
 
-user=> (chunked-seq? (seq (range 1000)))
-true
+(chunked-seq? (seq (range 1000)))
+;; => true
 
-user=> (chunked-seq? (iterate inc 10))
-false
+(chunked-seq? (iterate inc 10))
+;; => false
 
-user=> (chunked-seq? (seq (iterate inc 10)))
-false
+(chunked-seq? (seq (iterate inc 10)))
+;; => false
