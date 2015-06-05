cc44ab3de638651d4c297cb2ea35c55437f1460a
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:19:21 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/areduce/examples/1723213484.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/areduce/examples/1723213484.clj
@@ -1,8 +1,9 @@
 ;; This should be about as quick as summing up a array of floats in java.
 
-user=> (defn asum [#^floats xs]
-         (areduce xs i ret (float 0)
-                  (+ ret (aget xs i))))
+(defn asum [#^floats xs]
+  (areduce xs i ret (float 0)
+           (+ ret (aget xs i))))
+;; => #'user/asum
 
-user=> (asum (float-array [1 2 3]))
-6.0
+(asum (float-array [1 2 3]))
+;; => 6.0
