510826ac1bf70fa6ac605c72f4e8543be6743577
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:19:32 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/array-map/examples/1948816612.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/array-map/examples/1948816612.clj
@@ -1,3 +1,3 @@
-user=> (keys (assoc (array-map :foo 10 :bar 20) :baz 30))
-(:baz :foo :bar)
-; baz is first; :foo and :bar follow the order given to array-map
+(keys (assoc (array-map :foo 10 :bar 20) :baz 30))
+;; => (:baz :foo :bar)
+;; baz is first; :foo and :bar follow the order given to array-map
