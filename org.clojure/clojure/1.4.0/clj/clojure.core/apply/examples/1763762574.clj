a43b0ee3c253a122d4866ad0c48ca52bb6d0229b
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:18:41 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/apply/examples/1763762574.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/apply/examples/1763762574.clj
@@ -1,6 +1,6 @@
 ;; If you were to try
-user=> (max [1 2 3])
-[1 2 3]
+(max [1 2 3])
+;; => [1 2 3]
 
 ;; You would get '[1 2 3]' for the result. In this case, 'max' has received one
 ;; vector argument, and the largest of its arguments is that single vector.
@@ -8,7 +8,7 @@ user=> (max [1 2 3])
 ;; If you would like to find the largest item **within** the vector, you would need
 ;; to use `apply`
 
-user=> (apply max [1 2 3])
-3
+(apply max [1 2 3])
+;; => 3
 
 ;; which is the same as (max 1 2 3)
