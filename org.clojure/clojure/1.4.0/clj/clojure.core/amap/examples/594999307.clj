e0524e7e51eef540a54a2c082ca323cf36624cd2
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 14:28:56 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/amap/examples/594999307.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/amap/examples/594999307.clj
@@ -1,11 +1,12 @@
 (def an-array (int-array 25000 (int 0)))
+;; => #'user/an-array
 
-user=> (time (amap ^ints an-array 
-                   idx 
-                   ret 
-                   (+ (int 1) 
-                      (aget ^ints an-array idx))))
-
-"Elapsed time: 14.708653 msecs"
+(time (amap ^ints an-array 
+            idx 
+            ret 
+            (+ (int 1) 
+               (aget ^ints an-array idx))))
+;; > "Elapsed time: 14.708653 msecs"
+;; => nil
 
 ;; Note: without type hinting the performance of would not be good.
