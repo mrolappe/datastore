d69354490eb2168005526fabadbfb77973ab004b
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 14:28:13 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/alter-var-root/examples/1575197744.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/alter-var-root/examples/1575197744.clj
@@ -2,15 +2,15 @@
   "Squares a number"
   (* n n))
 
-user=> (sqr 5)
-25
+(sqr 5)
+;; => 25
 
-user=> (alter-var-root 
-         (var sqr)                     ; var to alter
-         (fn [f]                       ; fn to apply to the var's value
-           #(do (println "Squaring" %) ; returns a new fn wrapping old fn
-                (f %))))
+(alter-var-root 
+ (var sqr)                     ; var to alter
+ (fn [f]                       ; fn to apply to the var's value
+   #(do (println "Squaring" %) ; returns a new fn wrapping old fn
+        (f %))))
 
-user=> (sqr 5)
-Squaring 5
-25
+(sqr 5)
+;; > Squaring 5
+;; => 25
