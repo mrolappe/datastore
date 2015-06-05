b748db3400e6926365f571e8fb9994958df9db9c
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:23:29 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/bean/examples/1881433976.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/bean/examples/1881433976.clj
@@ -1,8 +1,8 @@
-user=> (import java.util.Date)
-java.util.Date
+(import java.util.Date)
+;; => java.util.Date
 
-user=> (def *now* (Date.))
-#'user/*now*
+(def *now* (Date.))
+;; => #'user/*now*
 
-user=> (bean *now*)
-{:seconds 57, :date 13, :class java.util.Date, :minutes 55, :hours 17, :year 110, :timezoneOffset -330, :month 6, :day 2, :time 1279023957492}
+(bean *now*)
+;; => {:seconds 57, :date 13, :class java.util.Date, :minutes 55, :hours 17, :year 110, :timezoneOffset -330, :month 6, :day 2, :time 1279023957492}
