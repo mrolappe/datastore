22cfdc902c5c6491786f5f48dc45881b26924369
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:21:06 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/aset-char/examples/1253581826.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/aset-char/examples/1253581826.clj
@@ -1,12 +1,14 @@
 ;; create an array of 10 characters (initially set to blank by default)
 ;; and set one of the elements to the character "a"
 
-user=> (def cs (char-array 10))
-#'user/cs
-user=> (vec cs)
-[\  \  \  \  \  \  \  \  \  \ ]
-user=> (aset-char cs 3 \a)
-\a
-user=> (vec cs)
-[\  \  \  \a \  \  \  \  \  \ ]
-user=>
+(def cs (char-array 10))
+;; => #'user/cs
+
+(vec cs)
+;; => [\  \  \  \  \  \  \  \  \  \ ]
+
+(aset-char cs 3 \a)
+;; => \a
+
+(vec cs)
+;; => [\  \  \  \a \  \  \  \  \  \ ]
