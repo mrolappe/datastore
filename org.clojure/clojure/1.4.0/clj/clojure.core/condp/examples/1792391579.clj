f325175d268e6ddf4d467624f4816267116a3518
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:35:45 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/condp/examples/1792391579.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/condp/examples/1792391579.clj
@@ -1,20 +1,29 @@
 ;; Taken from the excellent clojure tutorial:
 ;; http://java.ociweb.com/mark/clojure/article.html
 
-user=> (print "Enter a number: ")
-user=> (flush) ; stays in a buffer otherwise
-user=> (let [reader (java.io.BufferedReader. *in*) ; stdin
-             line (.readLine reader)
-             value (try
-                     (Integer/parseInt line)
-                     (catch NumberFormatException e line))] ;use string val if not int
-         (println
-           (condp = value
-             1 "one"
-             2 "two"
-             3 "three"
-             (str "unexpected value, \"" value \")))
-         (println
-           (condp instance? value
-             Number (* value 2)
-             String (* (count value) 2))))
+(do (print "Enter a number: ")
+    ;; Java's stdout is line buffered
+    ;; So we have to force flush it
+    (flush)
+    
+    (let [reader (java.io.BufferedReader. *in*) ; stdin
+          line   (.readLine reader)
+          value  (try
+                   (Integer/parseInt line)
+                   (catch NumberFormatException e line))] ;use string val if not int
+      (println
+       (condp = value
+         1 "one"
+         2 "two"
+         3 "three"
+         (str "unexpected value, \"" value \")))
+      (println
+       (condp instance? value
+         Number (* value 2)
+         String (* (count value) 2))))
+    nil)
+;; > Enter a number:
+;; < 3
+;; > "three"
+;; > 6
+;; => nil
