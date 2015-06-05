bba54b7548ef13616a372524c294ed495190289e
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:18:25 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/apply/examples/1760860614.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/apply/examples/1760860614.clj
@@ -1,18 +1,17 @@
-user=> (def *strings* ["str1" "str2" "str3"])
-#'user/*strings*
+(def *strings* ["str1" "str2" "str3"])
+;; => #'user/*strings*
 
 ;; Oops!
-user=> (str *strings*)
-"[\"str1\" \"str2\" \"str3\"]"
+(str *strings*)
+;; => "[\"str1\" \"str2\" \"str3\"]"
 
 ;; Yay!
-user=> (apply str *strings*)
-"str1str2str3"
-user=>
+(apply str *strings*)
+;; => "str1str2str3"
 
 ;; Note the equivalence of the following two forms
-user=> (apply str ["str1" "str2" "str3"])
-"str1str2str3"
+(apply str ["str1" "str2" "str3"])
+;; => "str1str2str3"
 
-user=> (str "str1" "str2" "str3")
-"str1str2str3"
+(str "str1" "str2" "str3")
+;; => "str1str2str3"
