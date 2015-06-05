8cb56ca716545c22c6955ea6ed47f3c41d31ef8e
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 14:21:50 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/%27%3D/examples/156828739.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/%27%3D/examples/156828739.clj
@@ -1,8 +1,11 @@
-user=> (>= 2 1)
-true
-user=> (>= 2 2)
-true
-user=> (>= 1 2)
-false
-user=> (>= 6 5 4 3 2)
-true
+(>= 2 1)
+;; => true
+
+(>= 2 2)
+;; => true
+
+(>= 1 2)
+;; => false
+
+(>= 6 5 4 3 2)
+;; => true
