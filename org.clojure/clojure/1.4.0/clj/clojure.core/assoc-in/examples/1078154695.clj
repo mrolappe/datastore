fe3b10cab5460cc856b3c9c952e73bb28cbbe3eb
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:22:56 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/assoc-in/examples/1078154695.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/assoc-in/examples/1078154695.clj
@@ -1,16 +1,15 @@
-user=> (def users [{:name "James" :age 26}  {:name "John" :age 43}])
- 
-user=> (assoc-in users [1 :age] 44)
-[{:name "James", :age 26} {:name "John", :age 44}]
+(def users [{:name "James" :age 26}  {:name "John" :age 43}])
+;; => #'user/users
 
-user=> (assoc-in users [1 :password] "nhoJ")
-[{:name "James", :age 26} {:password "nhoJ", :name "John", :age 43}]
+(assoc-in users [1 :age] 44)
+;; => [{:name "James", :age 26} {:name "John", :age 44}]
+
+(assoc-in users [1 :password] "nhoJ")
+;; => [{:name "James", :age 26} {:password "nhoJ", :name "John", :age 43}]
 
 ;; Also (assoc m 2 {...}) or (conj m {...})
-user=> (assoc-in users [2] {:name "Jack" :age 19})  
-[{:name "James", :age 26} {:name "John", :age 43} {:name "Jack", :age 19}]
-
-
+(assoc-in users [2] {:name "Jack" :age 19})  
+;; => [{:name "James", :age 26} {:name "John", :age 43} {:name "Jack", :age 19}]
 
 
 ;; From http://clojure-examples.appspot.com/clojure.core/assoc-in
