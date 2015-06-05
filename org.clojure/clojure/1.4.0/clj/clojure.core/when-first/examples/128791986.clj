6e68f33a3c38c6e24108c690eb14aab1aa4db949
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:15:40 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/when-first/examples/128791986.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/when-first/examples/128791986.clj
@@ -1,6 +1,8 @@
-user=> (when-first [a [1 2 3]] a)
-1
-user=> (when-first [a []] a)
-nil
-user=> (when-first [a nil] a)
-nil
+(when-first [a [1 2 3]] a)
+;; => 1
+
+(when-first [a []] a)
+;; => nil
+
+(when-first [a nil] a)
+;; => nil
