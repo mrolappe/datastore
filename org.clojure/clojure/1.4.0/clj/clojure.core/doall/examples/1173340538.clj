f082bec167be12a7995f4bdd4367fa9b9fdf2300
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:50:00 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/doall/examples/1173340538.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/doall/examples/1173340538.clj
@@ -1,28 +1,28 @@
 ;; Nothing is printed because map returns a lazy-seq
-user=> (def foo (map println [1 2 3]))
-#'user/foo
+(def foo (map println [1 2 3]))
+;; => #'user/foo
 
 ;; Get the value of foo
-user=> foo
-1
-2
-3
-(nil nil nil)
+foo
+;; > 1
+;; > 2
+;; > 3
+;; => (nil nil nil)
 
 ;; doall forces the seq to be realized
-user=> (def foo (doall (map println [1 2 3])))
-1
-2
-3
-#'user/foo
+(def foo (doall (map println [1 2 3])))
+;; > 1
+;; > 2
+;; > 3
+;; => #'user/foo
 
 ;; Like above, but note the difference
-user=> foo
-(nil nil nil)
+foo
+;; => (nil nil nil)
 
 ;; Where
 (doall (map println [1 2 3]))
-1
-2
-3
-(nil nil nil)
+;; > 1
+;; > 2
+;; > 3
+;; => (nil nil nil)
