3556539fa73e30a6a44456ffd1c153a807b44842
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:48:46 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/dissoc/examples/1695188183.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/dissoc/examples/1695188183.clj
@@ -1,12 +1,12 @@
-user=> (dissoc {:a 1 :b 2 :c 3}) ; dissoc nothing 
-{:a 1, :b 2, :c 3} 
+(dissoc {:a 1 :b 2 :c 3}) ; dissoc nothing 
+;; => {:a 1, :b 2, :c 3} 
 
-user=> (dissoc {:a 1 :b 2 :c 3} :b) ; dissoc key :b
-{:a 1, :c 3} 
+(dissoc {:a 1 :b 2 :c 3} :b) ; dissoc key :b
+;; => {:a 1, :c 3} 
 
-user=> (dissoc {:a 1 :b 2 :c 3} :d) ; dissoc not existed key
-{:a 1, :b 2, :c 3} 
+(dissoc {:a 1 :b 2 :c 3} :d) ; dissoc not existed key
+;; => {:a 1, :b 2, :c 3} 
 
-user=> (dissoc {:a 1 :b 2 :c 3} :c :b) ; several keys at once
-{:a 1} 
+(dissoc {:a 1 :b 2 :c 3} :c :b) ; several keys at once
+;; => {:a 1} 
 
