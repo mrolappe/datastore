bcc228c41ecdfb70490a219b36a49123c11d2518
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:30:14 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/comp/examples/88165230.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/comp/examples/88165230.clj
@@ -1,5 +1,3 @@
-user=> (map
-         (comp - (partial + 3) (partial * 2))
-         [1 2 3 4])
-; returns
-(-5 -7 -9 -11)
+(map (comp - (partial + 3) (partial * 2))
+     [1 2 3 4])
+;; => (-5 -7 -9 -11)
