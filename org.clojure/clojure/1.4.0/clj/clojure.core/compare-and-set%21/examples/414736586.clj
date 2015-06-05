6ef867f4504a913e8bc2fc7c8d40a8b7e923f8e7
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:31:30 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/compare-and-set%21/examples/414736586.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/compare-and-set%21/examples/414736586.clj
@@ -1,10 +1,14 @@
-user=> (def a (atom 0))
-#'user/a
-user=> (compare-and-set! a 10 20)
-false
-user=> @a
-0
-user=> (compare-and-set! a 0 10)
-true
-user=> @a
-10
+(def a (atom 0))
+;; => #'user/a
+
+(compare-and-set! a 10 20)
+;; => false
+
+@a
+;; => 0
+
+(compare-and-set! a 0 10)
+;; => true
+
+@a
+;; => 10
