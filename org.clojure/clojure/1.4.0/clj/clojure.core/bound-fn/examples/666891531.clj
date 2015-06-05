58fbb9885b22f8933f7b0ebb88345fc6d5d312d6
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:27:15 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/bound-fn/examples/666891531.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/bound-fn/examples/666891531.clj
@@ -3,24 +3,24 @@
 (defn f [] (println *some-var*))
 
 ;; run f without a new binding
-user=> (f)
-nil
-nil
+(f)
+;; > nil
+;; => nil
 
 ;; run f with a new binding
-user=> (binding [*some-var* "hello"]
-         (f))
-hello
-nil
+(binding [*some-var* "hello"]
+  (f))
+;; > hello
+;; => nil
 
 ;; run f in a thread with a new binding
-user=> (binding [*some-var* "goodbye"]
-         (.start (Thread. f)))
-nil
-nil
+(binding [*some-var* "goodbye"]
+  (.start (Thread. f)))
+;; > nil
+;; => nil
 
 ;; run a bound f in a thread with a new binding
-user=> (binding [*some-var* "goodbye"]
-         (.start (Thread. (bound-fn [] (f)))))
-goodbye
-nil
+(binding [*some-var* "goodbye"]
+  (.start (Thread. (bound-fn [] (f)))))
+;; > goodbye
+;; => nil
