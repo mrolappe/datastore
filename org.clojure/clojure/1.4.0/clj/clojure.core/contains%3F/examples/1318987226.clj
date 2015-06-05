885e312ff4afcfd7efa0071334f038b357c3fccf
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:37:05 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/contains%3F/examples/1318987226.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/contains%3F/examples/1318987226.clj
@@ -1,39 +1,36 @@
 ;; `contains?` is straightforward for maps:
-user=> (contains? {:a 1} :a)
-true
-user=> (contains? {:a nil} :a)
-true
-user=> (contains? {:a 1} :b)
-false
+(contains? {:a 1} :a)
+;; => true
 
+(contains? {:a nil} :a)
+;; => true
+
+(contains? {:a 1} :b)
+;; => false
 
 ;; It's likely to surprise you for other sequences because it's 
 ;; about *indices*, not *contents*:
+(contains? [:a :b :c] :b)
+;; => false
 
-user=> (contains? [:a :b :c] :b)
-false
+(contains? [:a :b :c] 2)
+;; => true
 
-user=> (contains? [:a :b :c] 2)
-true
+(contains? "f" 0)
+;; => true
 
-user=> (contains? "f" 0)
-true
-
-user=> (contains? "f" 1)
-false
-
+(contains? "f" 1)
+;; => false
 
 ;; It can be applied to non-sequences:
-
-user=> (contains? 5 3)
-false
-
+(contains? 5 3)
+;; => false
 
 ;; Although lists are sequences, `contains?` seems to always return
 ;; `false` for them. (Clojure 1.1)
 ;;
 ;; Note, this no longer works in Clojure 1.5, throwing an 
 ;; IllegalArgumentException.
-
-user=> (contains? '(1 2 3) 1) => false
+(contains? '(1 2 3) 1)
+;; => false
 
