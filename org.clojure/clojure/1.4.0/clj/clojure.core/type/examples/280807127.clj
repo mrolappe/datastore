b65f4f091d0ea1037cee74076be0d67dfaa158d8
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:12:50 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/type/examples/280807127.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/type/examples/280807127.clj
@@ -1,48 +1,45 @@
 ;; Checking numbers
-user=> (type 10)
-java.lang.Long
+(type 10)
+;; => java.lang.Long
 
-user=> (type 10.0)
-java.lang.Double
-
+(type 10.0)
+;; => java.lang.Double
 
 ;; Checking collections
-user=> (type [10 20])
-clojure.lang.PersistentVector
+(type [10 20])
+;; => clojure.lang.PersistentVector
 
-user=> (type '(10 20))
-clojure.lang.PersistentList
+(type '(10 20))
+;; => clojure.lang.PersistentList
 
 
 ;; Checking other, but somewhat intuitive, forms
-user=> (type :a)
-clojure.lang.Keyword
+(type :a)
+;; => clojure.lang.Keyword
 
-user=> (type Thread)
-java.lang.Class
-
+(type Thread)
+;; => java.lang.Class
 
 ;; Checking a symbol
-user=> (type 'whatever)
-clojure.lang.Symbol
+(type 'whatever)
+;; => clojure.lang.Symbol
 
 ;; A surprise attack yields
-user=> (type clojure.lang.Symbol)
+(type clojure.lang.Symbol)
 ;; not such a surprising response
-java.lang.Class
-
+;; => java.lang.Class
 
 ;; Checking a function
-user=> (defn foo [] ("any string"))
-#'user/foo
-user=> (type foo)
-user$foo
+(defn foo [] ("any string"))
+;; => #'user/foo
 
+(type foo)
+;; => user$foo
 
 ;; Checking a macro
-user=> (type fn)
-user$fn
+(type fn)
+;; => user$fn
 
-user=> (type clojure.core/fn)
-java.lang.Exception: Can't take value of a macro: #'clojure.core/fn (NO_SOURCE_FILE:94)
+(type clojure.core/fn)
+;; => java.lang.Exception: Can't take value of a macro: #'clojure.core/fn (NO_SOURCE_FILE:94)
 
