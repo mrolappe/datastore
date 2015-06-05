3a40438a0e232d17959333db054d2c17b83fc59e
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-05 15:13:35 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/unchecked-negate-int/examples/2064018800.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/unchecked-negate-int/examples/2064018800.clj
@@ -1,10 +1,14 @@
-user=> (unchecked-negate-int 4)
--4
-user=> (unchecked-negate-int 0)
-0
-user=> (unchecked-negate-int -7)
-7
-user=> (unchecked-negate-int Integer/MAX_VALUE)
--2147483647
-user=> (unchecked-negate-int Integer/MIN_VALUE) ;overflow
--2147483648
+(unchecked-negate-int 4)
+;; => -4
+
+(unchecked-negate-int 0)
+;; => 0
+
+(unchecked-negate-int -7)
+;; => 7
+
+(unchecked-negate-int Integer/MAX_VALUE)
+;; => -2147483647
+
+(unchecked-negate-int Integer/MIN_VALUE) ;overflow
+;; => -2147483648
