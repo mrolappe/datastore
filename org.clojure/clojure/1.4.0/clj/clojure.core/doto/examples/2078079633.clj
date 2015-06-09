f23f294925d5783f1d940a1951180dc24a30bd0c
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-09 10:02:31 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/doto/examples/2078079633.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/doto/examples/2078079633.clj
@@ -1,11 +1,13 @@
 ;; quick demonstration of using a Collections function on the resulting ArrayList
 
-user=> (def al (doto (java.util.ArrayList.) (.add 11) (.add 3)(.add 7)))
-#'user/al
-user=> al
-#<ArrayList [11, 3, 7]>
-user=> (java.util.Collections/sort al)
-nil
-user=> al
-#<ArrayList [3, 7, 11]>
-user=>
+(def al (doto (java.util.ArrayList.) (.add 11) (.add 3)(.add 7)))
+;; => #'user/al
+
+al
+;; => #<ArrayList [11, 3, 7]>
+
+(java.util.Collections/sort al)
+;; => nil
+
+al
+;; => #<ArrayList [3, 7, 11]>
