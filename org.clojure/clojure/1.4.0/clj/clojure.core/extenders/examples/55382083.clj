9ce86545286afb1fd17b7b2bcf6727e786296006
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-09 10:05:05 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/extenders/examples/55382083.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/extenders/examples/55382083.clj
@@ -1,12 +1,14 @@
-user=> (defprotocol P (id [this]))
-P
-user=> (extend-protocol P 
-         String 
-         (id [this] this)
-         clojure.lang.Symbol 
-         (id [this] (name this))
-         clojure.lang.Keyword
-         (id [this] (name this)))
-nil
-user=> (extenders P)
-(java.lang.String clojure.lang.Symbol clojure.lang.Keyword)
+(defprotocol P (id [this]))
+;; => P
+
+(extend-protocol P 
+  String 
+  (id [this] this)
+  clojure.lang.Symbol 
+  (id [this] (name this))
+  clojure.lang.Keyword
+  (id [this] (name this)))
+;; => nil
+
+(extenders P)
+;; => (java.lang.String clojure.lang.Symbol clojure.lang.Keyword)
