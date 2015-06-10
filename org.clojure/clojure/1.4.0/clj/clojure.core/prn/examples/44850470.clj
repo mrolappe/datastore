0e85cbf194dbd7e232133387523baa21156ab4c8
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:02:40 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/prn/examples/44850470.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/prn/examples/44850470.clj
@@ -6,17 +6,20 @@
 ;; => #'user/items
 
 ;; prn outputs items in a machine-readable format, such as in a source
-                                        ; file. Note the double-quotes around the string "hello" and the escaped letter "c".
+;; file. Note the double-quotes around the string "hello" and the escaped
+;; letter "c".
 (prn items)
 ;; > ["hello" :a 1 (:b 2) \c {:d 4} #{5 6 7}]
 ;; => nil
 
-;; println is for human-readable output, like a report. Note the lack of quotes around the string "hello", and the unescaped letter "c". 
+;; println is for human-readable output, like a report. Note the lack of quotes
+;; around the string "hello", and the unescaped letter "c".
 (println items)
 ;; > [hello :a 1 (:b 2) c {:d 4} #{5 6 7}]
 ;; => nil
 
-;; pr-str produces a string with escaped punctuation, so that println yields the same result as the original prn call.
+;; pr-str produces a string with escaped punctuation, so that println yields the
+;; same result as the original prn call.
 (println (pr-str items))
 ;; > ["hello" :a 1 (:b 2) \c {:d 4} #{5 6 7}]
 ;; => nil
