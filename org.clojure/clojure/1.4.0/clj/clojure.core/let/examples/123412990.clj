7181b485139857671d017b743f41473dbea3eae3
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 15:35:56 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/let/examples/123412990.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/let/examples/123412990.clj
@@ -11,33 +11,32 @@
 ;; collection.
 
 ;; A basic use for a let:
-user=> (let [x 1] 
-         x)
-1
+(let [x 1] 
+  x)
+;; => 1
 
 ;; Note that the binding for the symbol y won't exist outside of the let:
-user=> (let [y 1] 
-         y)
-1
-user=> (prn y)
-java.lang.Exception: Unable to resolve symbol: y in this context (NO_SOURCE_FILE:7)
+(let [y 1] 
+  y)
+;; => 1
+
+(prn y)
+;; => java.lang.Exception: Unable to resolve symbol: y in this context (NO_SOURCE_FILE:7)
 
 ;; Another valid use of let:
-user=> (let [a 1 b 2] 
-         (+ a b))
-3
+(let [a 1 b 2] 
+  (+ a b))
+;; => 3
 
 ;; The forms in the vector can be more complex (this example also uses
 ;; the thread macro):
-user=> (let [c (+ 1 2)
-             [d e] [5 6]] 
-         (-> (+ d e) (- c)))
-8
+(let [c (+ 1 2)
+      [d e] [5 6]] 
+  (-> (+ d e) (- c)))
+;; => 8
 
 ;; The bindings for let need not match up (note the result is a numeric
 ;; type called a ratio):
-user=> (let [[g h] [1 2 3]] 
-         (/ g h))
-1/2
-
-;; From http://clojure-examples.appspot.com/clojure.core/let with permission.
+(let [[g h] [1 2 3]] 
+  (/ g h))
+;; => 1/2
