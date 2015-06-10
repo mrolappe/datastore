1168981936f2bd5e3c98096f34a3f8f4cc7e061d
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:08:20 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/rand-nth/examples/731093050.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/rand-nth/examples/731093050.clj
@@ -1,7 +1,8 @@
-user=> (def food [:ice-cream :steak :apple])
-#'user/food
+(def food [:ice-cream :steak :apple])
+;; => #'user/food
 
-user=> (rand-nth food)
-:apple
-user=> (rand-nth food)
-:ice-cream
+(rand-nth food)
+;; => :apple
+
+(rand-nth food)
+;; => :ice-cream
