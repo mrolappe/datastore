9233f0dc054b4273cc1fa9cda044c80560eb7b0b
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 15:43:20 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/merge-with/examples/946276235.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/merge-with/examples/946276235.clj
@@ -1,7 +1,6 @@
 ;; merge two maps using the addition function
 
-user=> (merge-with + 
-                   {:a 1  :b 2}
-                   {:a 9  :b 98 :c 0})
-    
-{:c 0, :a 10, :b 100}
+(merge-with + 
+            {:a 1  :b 2}
+            {:a 9  :b 98 :c 0})
+;; => {:c 0, :a 10, :b 100}
