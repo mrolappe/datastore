5e624b48d227b52f5be17ab2412187f77bb9bd79
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 15:57:11 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/partial/examples/740418171.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/partial/examples/740418171.clj
@@ -1,5 +1,8 @@
-user=> (def to-english (partial clojure.pprint/cl-format nil "~@(~@[~R~]~^ ~A.~)"))
-#'user/to-english
+(require '[clojure.pprint])
+;; => nil
 
-user=> (to-english 1234567890)
-"One billion, two hundred thirty-four million, five hundred sixty-seven thousand, eight hundred ninety"
+(def to-english (partial clojure.pprint/cl-format nil "~@(~@[~R~]~^ ~A.~)"))
+;; => #'user/to-english
+
+(to-english 1234567890)
+;; => "One billion, two hundred thirty-four million, five hundred sixty-seven thousand, eight hundred ninety"
