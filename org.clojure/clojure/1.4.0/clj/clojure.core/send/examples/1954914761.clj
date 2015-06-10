6a570fffdded1f1897bcd4706752c0d967afc712
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:17:09 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/send/examples/1954914761.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/send/examples/1954914761.clj
@@ -4,12 +4,11 @@
 @my-agent
 ;; => 100
 
-;; Note the following happens asynchronously in a thread
-;; pool
+;; Note the following happens asynchronously in a thread pool
 (send my-agent + 100)
 ;; => #<Agent@5afc0f5: 200>
 
-;; Assuming the addition has completed the value will
-;; now be updated when we look at it.
+;; Assuming the addition has completed the value will now be updated when we
+;; look at it.
 @my-agent
 ;; => 200
