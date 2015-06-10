d1bb014c175ebf21899446d714b5b4477db333c2
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 15:58:59 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/pop%21/examples/1124539334.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/pop%21/examples/1124539334.clj
@@ -3,23 +3,32 @@
 ;; value and continuing to modify the original transient set.  See examples for
 ;; assoc! and dissoc! for more discussion and examples of this.
 
-user=> (def foo (transient [1 2 3]))
-#'user/foo
-user=> (count foo)
-3
-user=> (def foo (pop! foo))
-#'user/foo
-user=> foo
-#<TransientVector clojure.lang.PersistentVector$TransientVector@1638fff7>
-user=> (count foo)
-2
-user=> (def foo (pop! foo))
-#'user/foo
-user=> (count foo)
-1
-user=> (def foo (persistent! foo))
-#'user/foo
-user=> (count foo)
-1
-user=> foo
-[1]
+(def foo (transient [1 2 3]))
+;; => #'user/foo
+
+(count foo)
+;; => 3
+
+(def foo (pop! foo))
+;; => #'user/foo
+
+foo
+;; => #<TransientVector clojure.lang.PersistentVector$TransientVector@1638fff7>
+
+(count foo)
+;; => 2
+
+(def foo (pop! foo))
+;; => #'user/foo
+
+(count foo)
+;; => 1
+
+(def foo (persistent! foo))
+;; => #'user/foo
+
+(count foo)
+;; => 1
+
+foo
+;; => [1]
