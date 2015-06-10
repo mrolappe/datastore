672c6ff5a7a4c6201212c40ce1273df95ff95dd2
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:20:42 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/some/examples/1731849346.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/some/examples/1731849346.clj
@@ -1,7 +1,9 @@
 
-user=> (some true? [false false false])
-nil
-user=> (some true? [false true false])
-true
-user=> (some true? [true true true])
-true
+(some true? [false false false])
+;; => nil
+
+(some true? [false true false])
+;; => true
+
+(some true? [true true true])
+;; => true
