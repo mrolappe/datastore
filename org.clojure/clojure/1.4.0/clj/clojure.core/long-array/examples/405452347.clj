c8eae5054d801e56a6f06b5122122f0d93b9b745
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 15:40:01 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/long-array/examples/405452347.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/long-array/examples/405452347.clj
@@ -2,14 +2,17 @@
 ;; with the standard Java Arrays functions binarySearch and fill
 ;; note the needed coercions
 
-user=> (def is (long-array (range 3 20)))
-#'user/is
-user=> (vec is)
-[3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19]
-user=> (java.util.Arrays/binarySearch is (long 10))
-7
-user=> (java.util.Arrays/fill is 3 8 (long 99))
-nil
-user=> (vec is)
-[3 4 5 99 99 99 99 99 11 12 13 14 15 16 17 18 19]
-user=>
+(def is (long-array (range 3 20)))
+;; => #'user/is
+
+(vec is)
+;; => [3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19]
+
+(java.util.Arrays/binarySearch is (long 10))
+;; => 7
+
+(java.util.Arrays/fill is 3 8 (long 99))
+;; => nil
+
+(vec is)
+;; => [3 4 5 99 99 99 99 99 11 12 13 14 15 16 17 18 19]
