f919ae3109b69b82e95d706eec455bd8d054272c
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:27:12 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/str/examples/1107667022.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/str/examples/1107667022.clj
@@ -1,22 +1,26 @@
-user=> "some string"
 "some string"
+;; => "some string"
 
-user=> (str)
-""
-user=> (str nil)
-""
-user=> (str 1)
-"1"
-user=> (str 1 2 3)
-"123"
-user=> (str 1 'symbol :keyword)
-"1symbol:keyword"
+(str)
+;; => ""
+
+(str nil)
+;; => ""
+
+(str 1)
+;; => "1"
+
+(str 1 2 3)
+;; => "123"
+
+(str 1 'symbol :keyword)
+;; => "1symbol:keyword"
 
 ;; A very common usage of str is to apply it to an existing collection:
-user=> (apply str [1 2 3])
-"123"
+(apply str [1 2 3])
+;; => "123"
 
 ;; compare it with:
-user=> (str [1 2 3])
-"[1 2 3]"
+(str [1 2 3])
+;; => "[1 2 3]"
 
