01960e12189b2841a68654926bf802f4927ffd6f
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:06:36 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/promise/examples/1973038854.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/promise/examples/1973038854.clj
@@ -1,11 +1,16 @@
 (def x (promise))
 ;; => #'user/x
-;; Trying to deref at this point will make your repl wait forever
+
+;; Dereferencing a promise blocks on a value being delivered to that promise. So
+;; trying to deref x at this point will make your repl wait forever since
+;; nothing has been delivered to x yet.
 
 (deliver x 100)
 ;; => #<core$promise$reify__5534@4369a50b: 100>
 
-;; the promise has been delivered, deref x will return immediately
+;; x has now had a value delivered, so we can dereference it and we expect to
+;; get back the 100 we delivered..
+
 @x
 ;; => 100
 
