9f3dc00b106b3f48b0aac51b01ddd6a317348be2
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:17:29 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/send-off/examples/1953687165.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/send-off/examples/1953687165.clj
@@ -1,15 +1,16 @@
-user=> (def my-agent (agent ""))
-#'user/my-agent
-user=> @my-agent
-""
+(def my-agent (agent ""))
+;; => #'user/my-agent
+
+@my-agent
+;; => ""
 
 ;; Note the following happens asynchronously in a thread
 ;; pool
-user=> (send my-agent #(slurp %2) "file.txt")
-#<Agent@13c6641: "">
+(send my-agent #(slurp %2) "file.txt")
+;; => #<Agent@13c6641: "">
 
 ;; Assuming the action has been invoked the value will
 ;; now be updated when we look at it.
-user=> @my-agent
-"file contents"
+@my-agent
+;; => "file contents"
 
