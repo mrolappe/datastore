eff4bcf113e068c69814b6141928b5040c717d29
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 15:22:51 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/into-array/examples/531227876.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/into-array/examples/531227876.clj
@@ -1,7 +1,7 @@
 ;; Creating an empty array defaults to Object[]
-user=> (into-array [])
-#<Object[] [Ljava.lang.Object;@21f1151f>
+(into-array [])
+;; => #<Object[] [Ljava.lang.Object;@21f1151f>
 
 ;; However, the type of an empty array can be coerced
-user=> (into-array String [])
-#<String[] [Ljava.lang.String;@578baf24>
+(into-array String [])
+;; => #<String[] [Ljava.lang.String;@578baf24>
