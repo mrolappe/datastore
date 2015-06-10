f38027ec4a652789d779797557e5ff33992b034f
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:13:13 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/remove-ns/examples/1226472884.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/remove-ns/examples/1226472884.clj
@@ -3,11 +3,12 @@
 (create-ns 'my-new-namespace)
 ;; => #<Namespace my-new-namespace>
 
-;; removing a namespace will give you the namespace you just deleted, if one existed
+;; removing a namespace will give you the namespace you just deleted, if one
+;; existed
 (remove-ns 'my-new-namespace)
 ;; => #<Namespace my-new-namespace>
 
-;; removing a namespace that does not exist, will tell you that nothing was removed, 
-;; by returning nil, and won't give any errors
+;; removing a namespace that does not exist, will tell you that nothing was
+;; removed, by returning nil, and won't give any errors
 (remove-ns 'my-new-namespace)
 ;; => nil
