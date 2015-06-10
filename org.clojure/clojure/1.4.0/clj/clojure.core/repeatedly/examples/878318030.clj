2e7ddec303d8fb0cd2a00d7aaac16a893cbb43d4
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:14:36 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/repeatedly/examples/878318030.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/repeatedly/examples/878318030.clj
@@ -1,11 +1,11 @@
-;these two functions are equivalent 
+;; these two functions are equivalent 
 
-user=> (take 5 (repeatedly #(rand-int 11)))
-(6 6 3 9 8)
+(take 5 (repeatedly #(rand-int 11)))
+;; => (6 6 3 9 8)
 
-user=> (repeatedly 5 #(rand-int 11))
-(1 8 6 9 6)
+(repeatedly 5 #(rand-int 11))
+;; => (1 8 6 9 6)
 
-;compare with repeat
-user=> (repeat 5 (rand-int 100))
-(94 94 94 94 94)
+;; compare with repeat
+(repeat 5 (rand-int 100))
+;; => (94 94 94 94 94)
