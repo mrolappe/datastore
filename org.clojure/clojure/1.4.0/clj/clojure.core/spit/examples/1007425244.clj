d6dad57dfc7c04a22d4275e3be07b22c067a44b7
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:26:20 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/spit/examples/1007425244.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/spit/examples/1007425244.clj
@@ -1,11 +1,10 @@
-user=> (spit "event.log" "test 1\n" :append true)
-nil
+(spit "event.log" "test 1\n" :append true)
+;; => nil
 
-user=> (spit "event.log" "test 2\n" :append true)
-nil
+(spit "event.log" "test 2\n" :append true)
+;; => nil
 
-user=> (println (slurp "event.log"))
-test 1
-test 2
-
-nil
+(println (slurp "event.log"))
+;; > test 1
+;; > test 2
+;; => nil
