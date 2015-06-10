55cd41dbcbaaf4b22682d71dc506b9be674ad9b6
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 15:18:46 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/identical%3F/examples/741201583.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/identical%3F/examples/741201583.clj
@@ -1,9 +1,12 @@
-user=> (def x {:foo 1, :bar -3})
-#'user/x
-user=> (def y {:foo 1, :bar -3})
-#'user/y
+(def x {:foo 1, :bar -3})
+;; => #'user/x
+
+(def y {:foo 1, :bar -3})
+;; => #'user/y
+
 ;; Values are equal, but different objects were constructed
-user=> (= x y)
-true
-user=> (identical? x y)
-false
+(= x y)
+;; => true
+
+(identical? x y)
+;; => false
