ede0107de022719b95c5482a3f04a5d4f2200c77
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-10 16:01:51 -0700

    Reformat for style

--- org.clojure/clojure/1.4.0/clj/clojure.core/print-method/examples/1472721355.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/print-method/examples/1472721355.clj
@@ -1,12 +1,14 @@
 (deftype XYZ [])
 
-; without custom print-method defined:
-user=> (prn (XYZ.))
-#<XYZ user.XYZ@2670d85b> 
+;; without custom print-method defined:
+(prn (XYZ.))
+;; > #<XYZ user.XYZ@2670d85b> 
+;; => nil
 
 (defmethod print-method XYZ [v ^java.io.Writer w]
   (.write w "<<-XYZ->>"))
 
-; with print-method
-user=> (prn (XYZ.))
-<<-XYZ->>
+;; with print-method
+(prn (XYZ.))
+;; > <<-XYZ->>
+;; => nil
