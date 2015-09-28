c3c866ccafb8891216af4795e710399526f6d217
Reid D McKenzie <me@arrdem.com>
2015-09-28 00:35:14 -0500

    Remove AndyF's header
    Since it's now the notes.

--- org.clojure/clojure/1.4.0/clj/clojure.core/read/examples/1791028431.clj
+++ org.clojure/clojure/1.4.0/clj/clojure.core/read/examples/1791028431.clj
@@ -1,23 +1,3 @@
-;; WARNING: You SHOULD NOT use clojure.core/read or
-;; clojure.core/read-string to read data from untrusted sources.  They
-;; were designed only for reading Clojure code and data from trusted
-;; sources (e.g. files that you know you wrote yourself, and no one
-;; else has permission to modify them).
-
-;; Instead, either:
-
-;; (1) Use another data serialization format such as JSON, XML,
-;; etc. and a library for reading them that you trust not to have
-;; vulnerabilities, or
-
-;; (2) if you want a serialization format that can be read safely and
-;; looks like Clojure data structures, use edn
-;; (https://github.com/edn-format/edn).  For Clojure 1.3 and later,
-;; the tools.reader contrib library provides an edn reader
-;; (http://github.com/clojure/tools.reader).  There is also
-;; clojure.edn/read and clojure.edn/read-string provided in Clojure
-;; 1.5.
-
 ;; You definitely should not use clojure.core/read or read-string if
 ;; *read-eval* has its default value of true, because an attacker
 ;; could cause your application to execute arbitrary code while it is
