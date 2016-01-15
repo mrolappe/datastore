86fe58f59f719587f69cdf6793fb6eb655ad2bc1
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-01-14 18:15:55 -0600

    Add a cond-> example

---
+++ org.clojure/clojure/1.5.0/clj/clojure.core/cond-%3E/examples/cond-inc.clj
@@ -0,0 +1,17 @@
+(defn foo [x]
+  {:pre  [(number? x)]
+   :post [(number? x)]}
+  (cond-> x
+    (even? x)
+    ,,(inc)
+
+    :true
+    ,,(* 2)))
+
+;; => #'user/foo
+
+(foo 3)
+;; => 6
+
+(foo 4)
+;; => 10
